186b0e0c249749cbfdbf49c97d806e0a206405c3
Dylan Thorne <dylan.thorne@gmail.com>
2022-10-24 16:36:01 +1100

    reference/collection wip

---
+++ src/packages/graphql-api/src/schema/breeder/entity.ts
@@ -0,0 +1,24 @@
+import { AuthorizeAccess, BaseLoaders, GraphQLEntity } from '@exogee/base-resolver';
+import { Breeder as RestBreeder } from './entity';
+import { Field, ID, ObjectType, Root } from 'type-graphql';
+
+import { Dog } from '../dog';
+
+@ObjectType('Breeder')
+@AuthorizeAccess({})
+export class Breeder extends GraphQLEntity<RestBreeder> {
+	@Field(() => ID)
+	id!: string;
+
+	@Field(() => String)
+	name!: string;
+
+	@Field(() => [Dog])
+	async dogs(@Root() breeder: Breeder) {
+		return BaseLoaders.loadByRelatedId({
+			gqlEntityType: Dog,
+			relatedField: 'breeder',
+			id: breeder.id,
+		});
+	}
+}
--- src/packages/graphql-api/src/schema/index.ts
+++ src/packages/graphql-api/src/schema/index.ts
@@ -3,7 +3,8 @@ import { HobbyResolver } from './hobby';
 import { UserResolver } from './user';
 import { SkillResolver } from './skill';
 import { DogResolver } from './dog';
+import { BreederResolver } from './breeder';
 
 // The Function type is the type that Type GraphQL expects, and it's fine
 // eslint-disable-next-line @typescript-eslint/ban-types
-export const resolvers: [Function, ...Function[]] = [UserResolver, HobbyResolver, SkillResolver, DogResolver];
+export const resolvers: [Function, ...Function[]] = [UserResolver, HobbyResolver, SkillResolver, DogResolver, BreederResolver];
