16186e510bf6bebcabd80f41f84b093d11fa86ea
Kevin Anderson <kevin@3sixtymedia.nl>
2022-11-09 15:29:30 +0100

    button improvements

--- ui/src/components/Button.tsx
+++ ui/src/components/Button.tsx
@@ -3,11 +3,25 @@ import style from "./Button.module.css";
 function Button({
   handleClick = () => null,
   children,
+  iconBefore,
 }: {
   handleClick?: Function;
-  children: JSX.Element;
+  children: JSX.Element | string;
+  iconBefore?: string;
 }) {
-  return <button className={style.button}>{children}</button>;
+  function hasIconBefore() {
+    if (iconBefore) {
+      return true;
+    }
+  }
+  return (
+    <button className={style.button}>
+      <>
+        {hasIconBefore() ? <img src={iconBefore} alt="Icon" /> : null}
+        {children}
+      </>
+    </button>
+  );
 }
 
 export default Button;
