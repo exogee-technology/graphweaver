a614252993ce3258c56fcbd2ec9d406f8ab3c113
Kevin Brown <kevin.brown@exogee.com>
2022-11-15 10:29:01 +1100

    Ran prettier.

--- src/packages/rest-entities/src/decorators/index.ts
+++ src/packages/rest-entities/src/decorators/index.ts
@@ -2,4 +2,4 @@ export * from './field';
 export * from './many-to-one';
 export * from './many-to-many';
 export * from './external-id-field';
-export * from './one-to-many'
+export * from './one-to-many';
