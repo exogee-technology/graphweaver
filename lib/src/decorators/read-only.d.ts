3c5ef35b188ee67d6c819aa301fc4c3353f62c61
Kevin Brown <kevin.brown@exogee.com>
2022-11-14 14:00:26 +1100

    Remove built artefacts and add to .gitignore so they don't reappear.

--- lib/src/decorators/read-only.d.ts
+++
@@ -1,6 +0,0 @@
-import 'reflect-metadata';
-export declare function ReadOnly(): {
-    (target: Function): void;
-    (target: Object, propertyKey: string | symbol): void;
-};
-export declare function isReadOnly(target: any): boolean;
