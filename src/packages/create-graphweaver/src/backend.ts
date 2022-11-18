7bbdfc12f70dad4e93d3428873970277f99a04af
Kye <kye.lewis@techin.site>
2022-11-18 13:51:06 +1100

    prettier

--- src/packages/create-graphweaver/src/backend.ts
+++ src/packages/create-graphweaver/src/backend.ts
@@ -1,24 +1,22 @@
-import { GRAPHWEAVER_TARGET_VERSION } from "./constants";
+import { GRAPHWEAVER_TARGET_VERSION } from './constants';
 
 export enum Backend {
-  MikroORM,
-  REST,
+	MikroORM,
+	REST,
 }
 
-export const packagesForBackend = (
-  backend: Backend
-): Record<string, string> => {
-  switch (backend) {
-    case Backend.MikroORM:
-      return {
-        "@exogee/graphweaver-mikroorm": GRAPHWEAVER_TARGET_VERSION,
-        "@exogee/graphweaver-apollo": GRAPHWEAVER_TARGET_VERSION,
-        "@mikro-orm/core": "5.4.2",
-      };
+export const packagesForBackend = (backend: Backend): Record<string, string> => {
+	switch (backend) {
+		case Backend.MikroORM:
+			return {
+				'@exogee/graphweaver-mikroorm': GRAPHWEAVER_TARGET_VERSION,
+				'@exogee/graphweaver-apollo': GRAPHWEAVER_TARGET_VERSION,
+				'@mikro-orm/core': '5.4.2',
+			};
 
-    case Backend.REST:
-      return {
-        "@exogee/graphweaver-rest": GRAPHWEAVER_TARGET_VERSION,
-      };
-  }
+		case Backend.REST:
+			return {
+				'@exogee/graphweaver-rest': GRAPHWEAVER_TARGET_VERSION,
+			};
+	}
 };
