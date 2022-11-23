2ebc20baca26e3e318cd3ef916e3265feb39b2d6
Kye Lewis <kye.lewis@techin.site>
2022-11-24 10:22:56 +1100

    Update versions and fix types version issue in admin-ui (#17)

--- src/packages/create-graphweaver/src/backend.ts
+++ src/packages/create-graphweaver/src/backend.ts
@@ -1,10 +1,4 @@
-import {
-	GRAPHWEAVER_CORE_TARGET_VERSION,
-	GRAPHWEAVER_APOLLO_TARGET_VERSION,
-	GRAPHWEAVER_REST_TARGET_VERSION,
-	GRAPHWEAVER_MIKROORM_TARGET_VERSION,
-	MIKRO_ORM_TARGET_VERSION,
-} from './constants';
+import { GRAPHWEAVER_TARGET_VERSION, MIKRO_ORM_TARGET_VERSION } from './constants';
 
 export enum Backend {
 	MikroORMPostgres,
@@ -15,15 +9,15 @@ export const packagesForBackend = (backend: Backend): Record<string, string> => 
 	switch (backend) {
 		case Backend.MikroORMPostgres:
 			return {
-				'@exogee/graphweaver-mikroorm': GRAPHWEAVER_MIKROORM_TARGET_VERSION,
-				'@exogee/graphweaver-apollo': GRAPHWEAVER_APOLLO_TARGET_VERSION,
+				'@exogee/graphweaver-mikroorm': GRAPHWEAVER_TARGET_VERSION,
+				'@exogee/graphweaver-apollo': GRAPHWEAVER_TARGET_VERSION,
 				'@mikro-orm/core': MIKRO_ORM_TARGET_VERSION,
 				'@mikro-orm/postgresql': MIKRO_ORM_TARGET_VERSION,
 			};
 
 		case Backend.REST:
 			return {
-				'@exogee/graphweaver-rest': GRAPHWEAVER_REST_TARGET_VERSION,
+				'@exogee/graphweaver-rest': GRAPHWEAVER_TARGET_VERSION,
 			};
 	}
 };
