9c68e5a392a41e86b02b197fc6bc295e5e258c7f
Kevin Brown <kevin.brown@exogee.com>
2022-11-25 17:24:04 +1100

    WIP commit.

---
+++ src/packages/admin-ui/src/components/side-bar/backend-row.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+import { ReactComponent as DatabaseIcon } from '~/assets/16-database.svg';
+import { Entity, useSchema } from '~/utils/use-schema';
+import { EntityRow } from './entity-row';
+import styles from './styles.module.css';
+
+export const BackendRow = ({
+	backend,
+	selectedEntity,
+	onEntitySelected,
+}: {
+	backend: string;
+	selectedEntity?: Entity;
+	onEntitySelected?: (entity: Entity) => any;
+}) => {
+	const { entitiesForBackend } = useSchema();
+	const [expanded, setExpanded] = useState(false);
+
+	return (
+		<ul key={backend} className={styles.entity}>
+			<li className={expanded ? styles.open : styles.closed}>
+				<a
+					href="/#"
+					onClick={(e) => {
+						e.preventDefault();
+						setExpanded(!expanded);
+					}}
+				>
+					<span>
+						<DatabaseIcon />
+					</span>
+					{backend}
+				</a>
+				<ul>
+					{entitiesForBackend(backend).map((entity) => (
+						<EntityRow
+							entity={entity}
+							key={entity.name}
+							handleClick={() => onEntitySelected?.(entity)}
+							selected={selectedEntity?.name === entity.name}
+						/>
+					))}
+				</ul>
+			</li>
+		</ul>
+	);
+};
--- src/packages/admin-ui/src/pages/home/component.tsx
+++ src/packages/admin-ui/src/pages/home/component.tsx
@@ -1,36 +1,33 @@
 import { useState } from 'react';
-
 import { SideBar, Table, Button, FilterButton } from '~/components';
-import mockData from '~/utils/mock-data.json';
-
-import styles from './styles.module.css';
-import { ReactComponent as DataSourcesIcon } from '~/assets/64-data-sources.svg';
 import { ReactComponent as OpenPlaygroundIcon } from '~/assets/16-open-external.svg';
 import { ReactComponent as FilterIcon } from '~/assets/16-filter.svg';
+import { Entity } from '~/utils/use-schema';
+import styles from './styles.module.css';
 
-const BlankSlate = () => (
-	<div id={styles.centerBlankSlate}>
-		<div className={styles.blankSlateWrapper}>
-			<DataSourcesIcon />
-			<h1>No data sources yet</h1>
+// const BlankSlate = () => (
+// 	<div id={styles.centerBlankSlate}>
+// 		<div className={styles.blankSlateWrapper}>
+// 			<DataSourcesIcon />
+// 			<h1>No data sources yet</h1>
 
-			<p className="subtext">
-				Connect data sources. See the <a href="/#">readme</a> for more details
-			</p>
-		</div>
-	</div>
-);
+// 			<p className="subtext">
+// 				Connect data sources. See the <a href="/#">readme</a> for more details
+// 			</p>
+// 		</div>
+// 	</div>
+// );
 
 const ToolBar = () => (
 	<div className={styles.toolBarWrapper}>
 		<div className="titleWrapper">
-			<h1>endpoint</h1>
-			<p className="subtext">somedomain.com/api/endpoint</p>
+			<h1>localhost</h1>
+			<p className="subtext">localhost:3000/graphql/v1</p>
 		</div>
 
 		<div className={styles.toolsWrapper}>
 			<input className={styles.search} type="search" name="search" placeholder="Search..." />
-			<FilterButton dropdown={true} iconBefore={<FilterIcon />}>
+			<FilterButton dropdown iconBefore={<FilterIcon />}>
 				Filter
 			</FilterButton>
 
@@ -54,16 +51,16 @@ const ToolBar = () => (
 	</div>
 );
 
-const MainScreen = () => (
-	<>
-		<SideBar />
-		<div className={styles.content}>
-			<ToolBar />
-			<Table />
-		</div>
-	</>
-);
+export const Home = () => {
+	const [selectedEntity, setSelectedEntity] = useState<Entity | undefined>();
 
-export const Home = ({ hasData }: { hasData: boolean }) => (
-	<div className={styles.wrapper}>{hasData ? <MainScreen /> : <BlankSlate />}</div>
-);
+	return (
+		<div className={styles.wrapper}>
+			<SideBar selectedEntity={selectedEntity} onEntitySelected={setSelectedEntity} />
+			<div className={styles.content}>
+				<ToolBar />
+				<Table selectedEntity={selectedEntity} />
+			</div>
+		</div>
+	);
+};
