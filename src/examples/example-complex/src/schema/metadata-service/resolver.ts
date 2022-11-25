d57f9d34b1551c080a7757a83b81e1d8c519e601
Kishore Balan <93233664+kishorebalan@users.noreply.github.com>
2022-11-25 14:42:18 +1100

    cleanup

--- src/examples/example-complex/src/schema/metadata-service/resolver.ts
+++ src/examples/example-complex/src/schema/metadata-service/resolver.ts
@@ -1,14 +1,9 @@
-import {
-	AuthorizedBaseFunctions,
-	createBaseResolver,
-	EntityMetadataMap,
-} from '@exogee/graphweaver';
+import { AuthorizedBaseFunctions, EntityMetadataMap } from '@exogee/graphweaver';
 import { ReferenceType } from '@exogee/graphweaver-mikroorm';
 import { getMetadataStorage, Query, Resolver } from 'type-graphql';
 import { AdminField } from './admin-field';
 import { AdminUiMetadata } from './entity';
 import { ObjectClassMetadata } from 'type-graphql/dist/metadata/definitions/object-class-metdata';
-import { GraphQLScalarType } from 'graphql';
 
 @Resolver((of) => AdminUiMetadata)
 @AuthorizedBaseFunctions()
@@ -36,7 +31,7 @@ export class AdminUiMetadataResolver {
 				const relatedObject = objectTypeData[entityName];
 				if (field.typeOptions.array) {
 					if (!relatedObject) {
-						throw new Error(`unknown entityName ${entityName}`);
+						throw new Error(`Unknown entityName ${entityName}`);
 					}
 					const relatedEntity = relatedObject.fields.find((field) => {
 						const fieldType = field.getType() as any;
