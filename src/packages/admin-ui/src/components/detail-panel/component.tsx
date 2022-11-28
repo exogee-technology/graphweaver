0a04b16defec981f6a513aca101baece3c57fc52
Kevin Brown <kevin.brown@exogee.com>
2022-11-28 19:29:26 +1100

    Fixed overly-cached navigate function. Now showing errors better.

--- src/packages/admin-ui/src/components/detail-panel/component.tsx
+++ src/packages/admin-ui/src/components/detail-panel/component.tsx
@@ -1,7 +1,15 @@
 import { ApolloQueryResult } from '@apollo/client';
 import React from 'react';
-import { Await, useLoaderData } from 'react-router-dom';
+import { Await, useAsyncError, useLoaderData } from 'react-router-dom';
 import styles from './styles.module.css';
+
+const DetailPanelError = () => {
+	const error = useAsyncError() as Error;
+
+	console.error(error);
+
+	return <p>Error!: {error.message}</p>;
+};
 
 export const DetailPanel = () => {
 	const { detail } = useLoaderData() as { detail: any };
@@ -10,7 +18,7 @@ export const DetailPanel = () => {
 
 	return (
 		<React.Suspense fallback={<p>Loading...</p>}>
-			<Await resolve={detail} errorElement={<p>Error!</p>}>
+			<Await resolve={detail} errorElement={<DetailPanelError />}>
 				{(detail: ApolloQueryResult<{ result: { id: string } }>) => (
 					<pre className={styles.wrapper}>{JSON.stringify(detail.data.result, null, 4)}</pre>
 				)}
