7423b536836045cbbf7ce0fd03c8dc6653c58e32
Kishore Balan <93233664+kishorebalan@users.noreply.github.com>
2022-11-28 16:56:05 +1100

    move apollo out of example-complex

--- src/packages/apollo/src/index.ts
+++ src/packages/apollo/src/index.ts
@@ -3,22 +3,39 @@ import { ApolloServer } from 'apollo-server-lambda';
 import { AdminUiMetadataResolver } from './metadata-service';
 import { buildSchemaSync } from 'type-graphql';
 import { formatGraphQLError } from './plugins/format-error';
+import { ConnectionOptions } from '@exogee/graphweaver-mikroorm';
+import {
+	ClearDatabaseContext,
+	ClearDataLoaderCache,
+	connectToDatabase,
+	DisableApolloServerPluginLandingPage,
+	LogErrors,
+	LogRequests,
+	MutexRequestsInDevelopment,
+} from './plugins';
+import { Cors } from './plugins/cors';
+import { logger } from '@exogee/logger';
 export * from './plugins';
+export * from 'apollo-server-core';
 
 export interface AdminMetadata {
 	enabled: boolean;
 	config?: any;
 }
+
 export interface GraphweaverConfig extends Config {
 	adminMetadata?: AdminMetadata;
 	plugins?: PluginDefinition[];
+	mikroOrmOptions: ConnectionOptions;
 }
 export default class Graphweaver {
 	server: ApolloServer;
 	private config: GraphweaverConfig = {
 		adminMetadata: { enabled: true },
+		mikroOrmOptions: { mikroOrmConfig: { entities: [] } },
 	};
 	constructor(config: GraphweaverConfig) {
+		logger.info(`Graphweaver constructor called`);
 		if (!config) {
 			throw new Error('Graphweaver config required');
 		}
@@ -26,19 +43,39 @@ export default class Graphweaver {
 			throw new Error('Graphweaver config resolvers required');
 		}
 		this.config = config;
+		const plugins: PluginDefinition[] = [
+			MutexRequestsInDevelopment,
+			LogRequests,
+			LogErrors,
+			connectToDatabase(this.config.mikroOrmOptions),
+			ClearDataLoaderCache,
+			ClearDatabaseContext,
+			Cors,
+			DisableApolloServerPluginLandingPage,
+			...(this.config.plugins || []),
+		];
 		const resolvers = (this.config.resolvers || []) as any;
 		if (this.config.adminMetadata?.enabled && this.config.resolvers) {
+			logger.info(`Graphweaver adminMetadata is enabled`);
 			resolvers.push(AdminUiMetadataResolver);
 		}
+		logger.info(`Graphweaver buildSchemaSync with ${resolvers.length} resolvers`);
 		const schema = buildSchemaSync({
 			resolvers,
 			authChecker: () => true,
 		});
+		logger.info(`Graphweaver starting ApolloServer`);
 		this.server = new ApolloServer({
 			...this.config,
+			plugins,
 			schema,
 			introspection: process.env.IS_OFFLINE === 'true',
 			formatError: this.config.formatError ?? formatGraphQLError,
 		});
+	}
+
+	public async init() {
+		// Do some async here if necessary
+		logger.info(`Graphweaver async called`);
 	}
 }
