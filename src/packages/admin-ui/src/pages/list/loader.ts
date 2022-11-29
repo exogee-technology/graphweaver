2bfbe85464ae956324a9678ec45db5b16ec9fcc9
Simon Hall <simon.hall@techin.site>
2022-11-29 11:35:42 +1000

    Replaced stub schema in loader.

--- src/packages/admin-ui/src/pages/list/loader.ts
+++ src/packages/admin-ui/src/pages/list/loader.ts
@@ -1,12 +1,17 @@
 import { LoaderFunctionArgs, defer } from 'react-router-dom';
-import { schema } from '~/utils/stub-data';
 import { getEntity, getEntityPage } from '~/utils/data-loading';
+import { SCHEMA_QUERY } from '~/utils/graphql';
+import { client } from '~/apollo';
 
 const cleaningPattern = /[^a-zA-Z0-9]/g;
 
+const {
+	data: { _graphweaver: schema },
+} = await client.query({ query: SCHEMA_QUERY });
+
 const entityByNameOrType = (entity: string | undefined) => {
 	const cleaned = entity?.replaceAll(cleaningPattern, '');
-	const selectedEntity = schema.find((e) => e.name === cleaned);
+	const selectedEntity = schema.find((e: any) => e.name === cleaned);
 
 	if (!selectedEntity) throw new Error(`Unknown entity ${entity}.`);
 
