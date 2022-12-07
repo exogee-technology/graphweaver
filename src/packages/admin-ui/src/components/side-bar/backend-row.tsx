9829a2e30789c57f876d2b57f13118de7140f0e5
Kevin Brown <kevin.brown@exogee.com>
2022-12-07 15:04:22 +1100

    Xero dashboard works.

--- src/packages/admin-ui/src/components/side-bar/backend-row.tsx
+++ src/packages/admin-ui/src/components/side-bar/backend-row.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import { ReactComponent as DatabaseIcon } from '~/assets/16-database.svg';
 import { ReactComponent as ChevronIcon } from '~/assets/16-chevron-down.svg';
-import { Entity, useSchema } from '~/utils/use-schema';
+import { useSchema } from '~/utils/use-schema';
 import { useSelectedEntity } from '~/utils/use-selected-entity';
 import { EntityRow } from './entity-row';
 import styles from './styles.module.css';
