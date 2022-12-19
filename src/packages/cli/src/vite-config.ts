e8cffa11f01bf73e2a39a1a9a80f718eb3e5e828
Kevin Brown <kevin.brown@exogee.com>
2022-12-19 14:55:52 +1100

    Allow dashboards config to be optional.

--- src/packages/cli/src/vite-config.ts
+++ src/packages/cli/src/vite-config.ts
@@ -2,12 +2,16 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import graphweaver from 'vite-plugin-graphweaver';
 import { InlineConfig } from 'vite';
+import path from 'path';
 
 export const viteConfig: (viteRootDirectory: string) => InlineConfig = (
 	viteRootDirectory: string
 ) => ({
 	configFile: false,
 	root: viteRootDirectory,
+	build: {
+		outDir: path.resolve(process.cwd(), 'dist', 'admin-ui'),
+	},
 	server: {
 		port: 8000,
 	},
