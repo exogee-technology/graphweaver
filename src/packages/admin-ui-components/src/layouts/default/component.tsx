deb9e745ec841fc6767c28535c7752fd98e09291
Rich Simpson <rich.simpson@exogee.com>
2023-02-08 18:09:38 +1100

    Moved DataContext to wrap DefaultLayout - - instead of Router so that RequireSchema is loaded first

--- src/packages/admin-ui-components/src/layouts/default/component.tsx
+++ src/packages/admin-ui-components/src/layouts/default/component.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SideBar } from '../../side-bar';
 import { Header } from '../../header';
 import { RequireSchema } from '../../require-schema';
 import styles from './styles.module.css';
+import { DataContext, DataStateByEntity } from '../../utils';
 
 export const DefaultLayout = ({
 	header,
@@ -10,17 +11,22 @@ export const DefaultLayout = ({
 }: {
 	header?: React.ReactNode;
 	children: React.ReactNode;
-}) => (
-	<RequireSchema>
-		<div className={styles.container}>
-			<header>
-				<Header>{header}</Header>
-			</header>
-			<nav>
-				<SideBar />
-			</nav>
-			<div className={styles.content}>{children}</div>
-			<footer className={styles.footer}>{/* TODO: */ ''}</footer>
-		</div>
-	</RequireSchema>
-);
+}) => {
+	const [entityState, setEntityState] = useState<DataStateByEntity>({});
+	return (
+		<RequireSchema>
+			<DataContext.Provider value={{ entityState, setEntityState }}>
+				<div className={styles.container}>
+					<header>
+						<Header>{header}</Header>
+					</header>
+					<nav>
+						<SideBar />
+					</nav>
+					<div className={styles.content}>{children}</div>
+					<footer className={styles.footer}>{/* TODO: */ ''}</footer>
+				</div>
+			</DataContext.Provider>
+		</RequireSchema>
+	);
+};
