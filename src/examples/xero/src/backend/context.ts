5df7b6a687a57ed550e246344a9c6c250c1f1913
Rich Simpson <rich.simpson@exogee.com>
2023-03-02 11:42:28 +1100

    Update context to handle filesystem token auth - as generated by connect

--- src/examples/xero/src/backend/context.ts
+++ src/examples/xero/src/backend/context.ts
@@ -2,20 +2,33 @@ import { ForbiddenError } from 'type-graphql';
 import { TokenSet } from 'xero-node';
 import { XeroBackendProvider } from '@exogee/graphweaver-xero';
 import { logger } from '@exogee/logger';
+import * as fs from 'fs/promises';
 
 export const context = async ({ event }) => {
 	// Let's ensure we start with this as the default for every request.
 	XeroBackendProvider.clearTokensAndProvider();
 
+	const readTokenFromFs = async () => JSON.parse(await fs.readFile('./token.json', 'utf-8'));
+	const writeTokenToFs = async (newToken: TokenSet) =>
+		await fs.writeFile('./token.json', JSON.stringify(newToken, null, 4), 'utf-8');
+	const expired = (token: TokenSet): boolean => {
+		return Math.max((+token.expires_at ?? Date.now()) - Date.now()) === 0;
+	};
+
+	let token: TokenSet;
 	const authHeader = event.headers['Authorization'];
 	if (!authHeader) {
-		logger.trace('No auth header found, throwing ForbiddenError');
-		throw new ForbiddenError();
+		// Read from token.json. NOTE: This does at no point set the authorization header...
+		token = await readTokenFromFs();
+		if (!token) {
+			logger.trace('No auth header found, throwing ForbiddenError');
+			throw new ForbiddenError();
+		}
 	}
 
 	try {
 		logger.trace('Trying to parse token.');
-		let token = new TokenSet(JSON.parse(authHeader));
+		token = new TokenSet(token) || new TokenSet(JSON.parse(authHeader));
 		logger.trace('Token parsed successfully.');
 
 		// We don't fully verify the JWT here, and it's (easily) possible to bypass these checks
@@ -33,9 +46,17 @@ export const context = async ({ event }) => {
 			throw new ForbiddenError();
 		}
 
-		if (token.expired()) {
-			logger.trace('Token is expired, throwing ForbiddenError');
-			throw new ForbiddenError();
+		// Calling token.expired() here throws error about 'refresh' not being defined (ffs). So use our own
+		// method here. See https://github.com/panva/node-openid-client/blob/main/lib/token_set.js
+		// if (token.expired()) {
+		if (expired(token)) {
+			// try to refresh
+			token = await XeroBackendProvider.refreshToken();
+			await writeTokenToFs(token);
+			if (!token) {
+				logger.trace('Token is expired, throwing ForbiddenError');
+				throw new ForbiddenError();
+			}
 		}
 
 		XeroBackendProvider.accessTokenProvider = {
@@ -45,6 +66,7 @@ export const context = async ({ event }) => {
 			},
 		};
 
+		// @todo: Where does this go?
 		return { token };
 	} catch (error) {
 		logger.error(error);
