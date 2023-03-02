c6415e005c96e560d4422a80b99f057dffba8721
Rich Simpson <rich.simpson@exogee.com>
2023-03-02 16:45:39 +1100

    Re-enable token processing in provider - calls to XeroClient methods don't work properly in context, - just use context to define set/get accessToken

--- src/examples/xero/src/backend/context.ts
+++ src/examples/xero/src/backend/context.ts
@@ -12,62 +12,72 @@ export const context = async ({ event }) => {
 	const writeTokenToFs = async (newToken: TokenSet) =>
 		await fs.writeFile('./token.json', JSON.stringify(newToken, null, 4), 'utf-8');
 	const expired = (token: TokenSet): boolean => {
-		return Math.max((+token.expires_at ?? Date.now()) - Date.now()) === 0;
+		return Math.max((+token.expires_at ?? Date.now()) - Date.now(), 0) === 0;
 	};
 
-	let token: TokenSet;
+	/*	let token: TokenSet;
 	const authHeader = event.headers['Authorization'];
 	if (!authHeader) {
-		// Read from token.json. NOTE: This does at no point set the authorization header...
-		token = await readTokenFromFs();
-		if (!token) {
-			logger.trace('No auth header found, throwing ForbiddenError');
+		// // Read from token.json. NOTE: This does at no point set the authorization header...
+		// logger.trace('Context: No auth header found, reading from filesystem');
+		// token = await readTokenFromFs();
+		// if (!token) {
+			logger.trace('Context: No auth header/fs token found, throwing ForbiddenError');
 			throw new ForbiddenError();
-		}
+		// }
 	}
-
+*/
 	try {
-		logger.trace('Trying to parse token.');
-		token = new TokenSet(token) || new TokenSet(JSON.parse(authHeader));
-		logger.trace('Token parsed successfully.');
+		/*
+		logger.trace('Context: Trying to parse token.');
+		token = //new TokenSet(token) || 
+			new TokenSet(JSON.parse(authHeader));
+		logger.trace('Context: Token parsed successfully.');
 
 		// We don't fully verify the JWT here, and it's (easily) possible to bypass these checks
 		// by crafting a request, but Xero will validate before it gives any data back anyway, this
 		// is just to to provide nice errors to the frontend for the common non-malicious scenarios.
 		// If a user crafts a payload to try to fool us it still won't work, but they'll get a different
 		// error message.
+		logger.trace('Context: Looking for Bearer type.');
 		if (token.token_type !== 'Bearer') {
-			logger.trace('Token type is not Bearer, throwing ForbiddenError');
+			logger.trace('Context: Token type is not Bearer, throwing ForbiddenError');
 			throw new ForbiddenError();
 		}
 
+		logger.trace('Context: Looking for access/id tokens.');
 		if (!token.access_token || !token.id_token) {
-			logger.trace('Token is missing access_token or id_token, throwing ForbiddenError');
+			logger.trace('Context: Token is missing access_token or id_token, throwing ForbiddenError');
 			throw new ForbiddenError();
 		}
 
 		// Calling token.expired() here throws error about 'refresh' not being defined (ffs). So use our own
 		// method here. See https://github.com/panva/node-openid-client/blob/main/lib/token_set.js
-		// if (token.expired()) {
-		if (expired(token)) {
+		if (token.expired()) {
+		// logger.trace('Context: checking expiry.');
+		// if (expired(token)) {
 			// try to refresh
+			logger.trace('Context: Token expired. Refreshing.');
 			token = await XeroBackendProvider.refreshToken();
+			logger.trace('Context: Token refreshed. Writing to filesystem.');
 			await writeTokenToFs(token);
 			if (!token) {
-				logger.trace('Token is expired, throwing ForbiddenError');
+				logger.trace('Context: Token is expired, could not refresh, throwing ForbiddenError');
 				throw new ForbiddenError();
 			}
 		}
-
+*/
 		XeroBackendProvider.accessTokenProvider = {
-			get: () => token,
-			set: async (newToken) => {
-				token = newToken;
-			},
+			// get: () => token,
+			// set: async (newToken) => {
+			// 	token = newToken;
+			// },
+			get: async () => await readTokenFromFs(),
+			set: async (newToken) => await writeTokenToFs(newToken),
 		};
 
 		// @todo: Where does this go?
-		return { token };
+		return { token: await readTokenFromFs() };
 	} catch (error) {
 		logger.error(error);
 		throw new ForbiddenError();
