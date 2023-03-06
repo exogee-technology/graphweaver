47d97419cfb81a4033e89013b1cd7a4ffed7bbed
Kevin Brown <kevin.brown@exogee.com>
2023-03-06 15:45:39 +1100

    Move auth code saving to custom page.

--- src/packages/admin-ui-components/src/layouts/default/component.tsx
+++ src/packages/admin-ui-components/src/layouts/default/component.tsx
@@ -3,7 +3,6 @@ import { SideBar } from '../../side-bar';
 import { Header } from '../../header';
 import { RequireSchema } from '../../require-schema';
 import styles from './styles.module.css';
-import { SaveAuthCodeFromQueryString } from '../../save-auth-code-from-query-string';
 
 export const DefaultLayout = ({
 	header,
@@ -12,20 +11,17 @@ export const DefaultLayout = ({
 	header?: React.ReactNode;
 	children: React.ReactNode;
 }) => (
-	// This makes sure we save the auth code from the query string if we're receiving one on any page.
-	<SaveAuthCodeFromQueryString>
-		{/* This makes sure the schema loads before any of the stuff trying to access data loads. */}
-		<RequireSchema>
-			<div className={styles.container}>
-				<header>
-					<Header>{header}</Header>
-				</header>
-				<nav>
-					<SideBar />
-				</nav>
-				<div className={styles.content}>{children}</div>
-				{/** @todo <footer className={styles.footer}></footer> */}
-			</div>
-		</RequireSchema>
-	</SaveAuthCodeFromQueryString>
+	// This makes sure the schema loads before any of the stuff trying to access data loads.
+	<RequireSchema>
+		<div className={styles.container}>
+			<header>
+				<Header>{header}</Header>
+			</header>
+			<nav>
+				<SideBar />
+			</nav>
+			<div className={styles.content}>{children}</div>
+			{/** @todo <footer className={styles.footer}></footer> */}
+		</div>
+	</RequireSchema>
 );
