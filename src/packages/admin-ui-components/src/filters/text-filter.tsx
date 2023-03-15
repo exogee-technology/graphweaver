b701187db88c743371617f419afe57785190a982
Rich Simpson <rich.simpson@exogee.com>
2023-03-15 15:24:13 +1100

    Implement Date Range and Numeric (Range) filters

--- src/packages/admin-ui-components/src/filters/text-filter.tsx
+++ src/packages/admin-ui-components/src/filters/text-filter.tsx
@@ -1,62 +1,58 @@
 import React, { useContext } from 'react';
 import { DataContext, DataStateByEntity, Select, SelectOption, useSchema } from '..';
 
-export const TextFilter = React.forwardRef(
-	<T extends { id: string }>(
-		{
-			fieldName,
-			entity,
-			onSelect,
-			selected,
-		}: {
-			fieldName: string;
-			entity: string;
-			// onSelect?: (fieldName: string, filter?: Filter) => void;
-			onSelect?: (fieldName: string, option?: SelectOption) => void;
-			selected?: SelectOption;
-		},
-		ref: any
-	) => {
-		const { entityByName } = useSchema();
-		const { entityState, setEntityState } = useContext(DataContext);
+interface TextFilterProps {
+	fieldName: string;
+	entity: string;
+	// onSelect?: (fieldName: string, filter?: Filter) => void;
+	onSelect?: (fieldName: string, option?: SelectOption) => void;
+	selected?: SelectOption;
+}
 
-		const entityType = entityByName(entity);
-		const data = (entityState as DataStateByEntity)[entity]?.data as T[];
-		const field = entityType?.fields.find((f) => f.name === fieldName);
-		let textOptions: SelectOption[] = [];
-		if (data && field && data.length > 0) {
-			// Get a unique, sorted list
-			textOptions = data
-				// yuk
-				.map((item) => (item as any)[field.name])
-				.reduce((arr: string[], item) => {
-					if (!arr.includes(item)) {
-						arr.push(item);
-					}
-					return arr;
-				}, [])
-				.sort((a, b) => a?.toLocaleLowerCase().localeCompare(b?.toLocaleLowerCase()))
-				// value = label, we don't care
-				.map((item) => ({ label: item, value: item }));
-		}
+export const TextFilter = <T extends { id: string }>({
+	fieldName,
+	entity,
+	onSelect,
+	selected,
+}: TextFilterProps) => {
+	const { entityByName } = useSchema();
+	const { entityState, setEntityState } = useContext(DataContext);
 
-		const onChange = (option?: SelectOption) => {
-			// option will be empty if 'clear' selected
-			if (!onSelect) return;
-			return onSelect(fieldName, option);
-		};
-
-		return (
-			<Select
-				key={fieldName}
-				value={selected}
-				options={textOptions}
-				placeholder={fieldName}
-				isClearable
-				clearSelection
-				ref={ref}
-				onChange={onChange}
-			/>
-		);
+	const entityType = entityByName(entity);
+	const data = (entityState as DataStateByEntity)[entity]?.data as T[];
+	const field = entityType?.fields.find((f) => f.name === fieldName);
+	let textOptions: SelectOption[] = [];
+	if (data && field && data.length > 0) {
+		// Get a unique, sorted list
+		textOptions = data
+			// yuk
+			.map((item) => (item as any)[field.name])
+			.reduce((arr: string[], item) => {
+				if (!arr.includes(item)) {
+					arr.push(item);
+				}
+				return arr;
+			}, [])
+			.sort((a, b) => a?.toLocaleLowerCase().localeCompare(b?.toLocaleLowerCase()))
+			// value = label, we don't care
+			.map((item) => ({ label: item, value: item }));
 	}
-);
+
+	const onChange = (option?: SelectOption) => {
+		// option will be empty if 'clear' selected
+		if (!onSelect) return;
+		return onSelect(fieldName, option);
+	};
+
+	return (
+		<Select
+			key={fieldName}
+			value={selected}
+			options={textOptions}
+			placeholder={fieldName}
+			isClearable
+			clearSelection
+			onChange={onChange}
+		/>
+	);
+};
---
+++ src/packages/admin-ui-components/src/utils/utils.ts
@@ -0,0 +1,8 @@
+export const isNumeric = (item: unknown): boolean => {
+	if (item === undefined || item === null) return false;
+	if (typeof item === 'bigint' || typeof item === 'number') return true;
+	if (typeof item === 'string') {
+		return !isNaN(+item);
+	}
+	return false;
+};
