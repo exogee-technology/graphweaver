55fbe93ced4f7420135b750f6e21ecd168b77d0c
Steve Keep <stephen.keep@techin.site>
2023-03-20 10:34:46 +1100

    add graphql to page

--- src/packages/admin-ui-components/src/playground/component.tsx
+++ src/packages/admin-ui-components/src/playground/component.tsx
@@ -1,3 +1,24 @@
+import GraphiQL from 'graphiql';
+import 'graphiql/graphiql.min.css';
+
 export const Playground = () => {
-	return <p>Playground Placeholder</p>;
+	return (
+		<>
+			<p>Playground Placeholder</p>
+			<GraphiQL
+				fetcher={async (graphQLParams) => {
+					const data = await fetch('https://swapi-graphql.netlify.app/.netlify/functions/index', {
+						method: 'POST',
+						headers: {
+							Accept: 'application/json',
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify(graphQLParams),
+						credentials: 'same-origin',
+					});
+					return data.json().catch(() => data.text());
+				}}
+			/>
+		</>
+	);
 };
