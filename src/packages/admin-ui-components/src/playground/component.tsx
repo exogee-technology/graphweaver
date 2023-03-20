a2b3c49f45d61852687503f90b4eb3d7ed97fcbf
Steve Keep <stephen.keep@techin.site>
2023-03-20 14:06:05 +1100

    added a fetch middleware

--- src/packages/admin-ui-components/src/playground/component.tsx
+++ src/packages/admin-ui-components/src/playground/component.tsx
@@ -5,14 +5,39 @@ import 'graphiql/graphiql.min.css';
 
 import { uri } from '../config';
 
-export const Playground = () => {
-	// There is probably a better way to do this
-	// We could have a global context with the auth information
-	// Then we only need to read the auth details in a single location
+// This Fetch Middleware does a few things:
+//   1. If there's something called `graphweaver-auth` in local storage, we need to send that to the server.
+//   2. If the server sends back a header called `X-Auth-Redirect` on any response, we need to redirect the user to that URL.
+//   3. If the server sends back a header called `Authorization` on any response, we need to update our `graphweaver-auth` local storage value with
+//      what we got from the server.
+const fetchMiddleware = async (url: RequestInfo | URL, init?: RequestInit) => {
 	const auth = localStorage.getItem('graphweaver-auth');
-	const fetcher = createGraphiQLFetcher({
-		url: uri,
-		headers: { ...(auth ? { Authorization: auth } : {}) },
+	const response = await fetch(url, {
+		...init,
+		headers: {
+			...init?.headers,
+			...(auth ? { Authorization: auth } : {}), // Feature 1 above ☝️
+		},
 	});
+
+	// Feature 2 above ☝️
+	// Do we have an auth redirect we need to do for OAuth?
+	const redirectHeader = response.headers.get('X-Auth-Redirect');
+	if (redirectHeader) window.location.href = redirectHeader;
+
+	// Feature 3 above ☝️
+	// Do we have an auth header we need to store?
+	const authHeader = response.headers.get('Authorization');
+	if (authHeader) localStorage.setItem('graphweaver-auth', authHeader);
+
+	return response;
+};
+
+const fetcher = createGraphiQLFetcher({
+	url: uri,
+	fetch: fetchMiddleware,
+});
+
+export const Playground = () => {
 	return <GraphiQL fetcher={fetcher} />;
 };
