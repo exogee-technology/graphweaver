c039773eb359fb9c58e6f550736ba853ebc145f3
Steve Keep <stephen.keep@techin.site>
2023-03-20 12:21:54 +1100

    add fetch comment

--- src/packages/admin-ui-components/src/playground/component.tsx
+++ src/packages/admin-ui-components/src/playground/component.tsx
@@ -5,10 +5,14 @@ import 'graphiql/graphiql.min.css';
 
 import { uri } from '../config';
 
-const auth = localStorage.getItem('graphweaver-auth');
-const fetcher = createGraphiQLFetcher({
-	url: uri,
-	headers: { ...(auth ? { Authorization: auth } : {}) },
-});
-
-export const Playground = () => <GraphiQL fetcher={fetcher} />;
+export const Playground = () => {
+	// There is probably a better way to do this
+	// We could have a global context with the auth information
+	// Then we only need to read the auth details in a single location
+	const auth = localStorage.getItem('graphweaver-auth');
+	const fetcher = createGraphiQLFetcher({
+		url: uri,
+		headers: { ...(auth ? { Authorization: auth } : {}) },
+	});
+	<GraphiQL fetcher={fetcher} />;
+};
