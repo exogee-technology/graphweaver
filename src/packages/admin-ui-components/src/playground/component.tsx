8040a5d3f3c62b3ab94a63920a401663fc6a0ebb
Steve Keep <stephen.keep@techin.site>
2023-03-20 12:00:34 +1100

    cleanup playground component

--- src/packages/admin-ui-components/src/playground/component.tsx
+++ src/packages/admin-ui-components/src/playground/component.tsx
@@ -1,25 +1,14 @@
 import GraphiQL from 'graphiql';
+import { createGraphiQLFetcher } from '@graphiql/toolkit';
+
 import 'graphiql/graphiql.min.css';
 
 import { uri } from '../config';
 
-export const Playground = () => {
-	const auth = localStorage.getItem('graphweaver-auth');
-	return (
-		<GraphiQL
-			fetcher={async (graphQLParams) => {
-				const data = await fetch(uri, {
-					method: 'POST',
-					headers: {
-						Accept: 'application/json',
-						'Content-Type': 'application/json',
-						...(auth ? { Authorization: auth } : {}),
-					},
-					body: JSON.stringify(graphQLParams),
-					credentials: 'same-origin',
-				});
-				return data.json().catch(() => data.text());
-			}}
-		/>
-	);
-};
+const auth = localStorage.getItem('graphweaver-auth');
+const fetcher = createGraphiQLFetcher({
+	url: uri,
+	headers: { ...(auth ? { Authorization: auth } : {}) },
+});
+
+export const Playground = () => <GraphiQL fetcher={fetcher} />;
