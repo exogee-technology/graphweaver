359e4a62876129c22fd095cfe2837c25158781f8
Steve Keep <stephen.keep@techin.site>
2023-03-22 09:19:27 +1100

    cleanup the clear filters button

--- src/packages/admin-ui-components/src/filters/index.ts
+++ src/packages/admin-ui-components/src/filters/index.ts
@@ -2,6 +2,4 @@ export * from './enum-filter';
 export * from './text-filter';
 export * from './relationship-filter';
 export * from './date-range-filter';
-export * from './number-range-filter';
 export * from './numeric-filter';
-export * from './filter-selector';
