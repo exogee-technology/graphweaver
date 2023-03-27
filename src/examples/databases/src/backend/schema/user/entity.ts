fa6c140e12620ac1e12f69c292d9a847cc685302
Steve Keep <stephen.keep@techin.site>
2023-03-27 16:13:48 +1100

    link the two entities together

--- src/examples/databases/src/backend/schema/user/entity.ts
+++ src/examples/databases/src/backend/schema/user/entity.ts
@@ -1,7 +1,14 @@
-import { GraphQLEntity, AdminUISettings } from '@exogee/graphweaver';
+import {
+	GraphQLEntity,
+	AdminUISettings,
+	BaseLoaders,
+	SummaryField,
+	AdminUIFilterType,
+} from '@exogee/graphweaver';
 import { Field, ID, ObjectType } from 'type-graphql';
 
 import { User as OrmUser } from '../../entities';
+import { Task } from '../task';
 
 @ObjectType('User')
 export class User extends GraphQLEntity<OrmUser> {
@@ -10,9 +17,20 @@ export class User extends GraphQLEntity<OrmUser> {
 	@Field(() => ID)
 	id!: string;
 
+	@AdminUISettings({
+		filter: {
+			type: AdminUIFilterType.TEXT,
+		},
+	})
+	@SummaryField()
 	@Field(() => String)
 	username!: string;
 
+	@AdminUISettings({
+		filter: {
+			type: AdminUIFilterType.TEXT,
+		},
+	})
 	@Field(() => String)
 	email!: string;
 }
