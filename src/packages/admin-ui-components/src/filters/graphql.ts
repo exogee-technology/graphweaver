767e61d6154c2aff6f50e57a449b9616c56146b0
Steve Keep <stephen.keep@techin.site>
2023-04-03 14:36:21 +1000

    remove data context

--- src/packages/admin-ui-components/src/filters/graphql.ts
+++ src/packages/admin-ui-components/src/filters/graphql.ts
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client';
 import pluralize from 'pluralize';
+import { Entity } from '../utils';
 
 export const getRelationshipQuery = (entityName: string, summaryField?: string) => {
 	const pluralName = pluralize(entityName);
@@ -13,4 +14,23 @@ export const getRelationshipQuery = (entityName: string, summaryField?: string) 
       }
     }
   `;
+};
+
+export const queryForFilterText = (
+	entityName: string,
+	fieldName: string,
+	entityByType: (type: string) => Entity
+) => {
+	const entity = entityByType(entityName);
+	const pluralName = pluralize(entity.name);
+	const queryName = pluralName[0].toLowerCase() + pluralName.slice(1);
+
+	return gql`
+		query TextFilter {
+			result: ${queryName} {
+				id
+        ${fieldName}
+			}
+		}
+	`;
 };
