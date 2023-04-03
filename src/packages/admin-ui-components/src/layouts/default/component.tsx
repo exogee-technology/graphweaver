035f316c25e054735c91db743408c24bfb9f3e59
Steve Keep <stephen.keep@techin.site>
2023-04-03 14:36:21 +1000

    remove data context

--- src/packages/admin-ui-components/src/layouts/default/component.tsx
+++ src/packages/admin-ui-components/src/layouts/default/component.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
 import { SideBar } from '../../side-bar';
 import { Header } from '../../header';
 import { RequireSchema } from '../../require-schema';
 import styles from './styles.module.css';
-import { DataContext } from '../../utils';
 
 export const DefaultLayout = ({
 	header,
@@ -12,21 +10,18 @@ export const DefaultLayout = ({
 	header?: React.ReactNode;
 	children: React.ReactNode;
 }) => {
-	const [entityState, setEntityState] = useState({});
 	return (
 		<RequireSchema>
-			<DataContext.Provider value={{ entityState, setEntityState }}>
-				<div className={styles.container}>
-					<header>
-						<Header>{header}</Header>
-					</header>
-					<nav>
-						<SideBar />
-					</nav>
-					<div className={styles.content}>{children}</div>
-					{/** @todo <footer className={styles.footer}></footer> */}
-				</div>
-			</DataContext.Provider>
+			<div className={styles.container}>
+				<header>
+					<Header>{header}</Header>
+				</header>
+				<nav>
+					<SideBar />
+				</nav>
+				<div className={styles.content}>{children}</div>
+				{/** @todo <footer className={styles.footer}></footer> */}
+			</div>
 		</RequireSchema>
 	);
 };
