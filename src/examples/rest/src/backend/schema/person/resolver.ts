acb84b8cc012a1fd82809ec20dbab372d9d12e6b
Steve Keep <stephen.keep@techin.site>
2023-04-06 16:23:42 +1000

    provide better fallback

--- src/examples/rest/src/backend/schema/person/resolver.ts
+++ src/examples/rest/src/backend/schema/person/resolver.ts
@@ -18,12 +18,10 @@ export class PersonResolver extends createBaseResolver(
 			const { results } = await fetch(`/people`);
 
 			for (const person of results) {
-				const paths = url
-					.parse(person.url)
-					.pathname?.split('/')
-					.filter((part) => part);
-				const id = paths?.[paths.length - 1];
-				(person as RestPeople & { id: string }).id = id || 'null';
+				const [_, __, id] = (url.parse(person.url).pathname?.split('/') || []).filter(
+					(part) => part
+				);
+				(person as RestPeople & { id: string }).id = id || person.url;
 			}
 
 			if (filter) {
