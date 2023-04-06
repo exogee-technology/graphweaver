d8a31c23a79b75b131cadb967c7e4af826575b34
Steve Keep <stephen.keep@techin.site>
2023-04-06 16:00:10 +1000

    remove complex examples

--- src/examples/complex/src/entities/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './mikroorm';
-export * from './rest';
-import { User, Skill, Hobby, Session, Migration, UserDog } from './mikroorm';
-
-export const mikroOrmEntities = [User, Skill, Hobby, Session, Migration, UserDog];
