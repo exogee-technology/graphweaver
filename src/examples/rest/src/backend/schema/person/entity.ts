b9e546066261775d13baa3207aea5cefa3b996b9
Steve Keep <stephen.keep@techin.site>
2023-04-06 11:34:34 +1000

    initial layout

---
+++ src/examples/rest/src/backend/schema/person/entity.ts
@@ -0,0 +1,16 @@
+import { GraphQLEntity, SummaryField } from '@exogee/graphweaver';
+import { Field, ID, ObjectType } from 'type-graphql';
+
+import { People as RestPeople } from '../../entities';
+
+@ObjectType('Person')
+export class Person extends GraphQLEntity<RestPeople> {
+	public dataEntity!: RestPeople;
+
+	@Field(() => ID)
+	id!: string;
+
+	@SummaryField()
+	@Field(() => String)
+	name!: string;
+}
