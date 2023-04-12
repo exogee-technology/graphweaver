cd4f36f18491912c8d94639b6ebac106c0c08d8b
Steve Keep <stephen.keep@techin.site>
2023-04-12 11:28:48 +1000

    add simple adjustable side bar

--- src/packages/admin-ui-components/src/layouts/default/component.tsx
+++ src/packages/admin-ui-components/src/layouts/default/component.tsx
@@ -1,7 +1,13 @@
+import { useEffect, useRef, useState } from 'react';
+
 import { SideBar } from '../../side-bar';
 import { Header } from '../../header';
 import { RequireSchema } from '../../require-schema';
 import styles from './styles.module.css';
+
+const SIDEBAR_MIN_WIDTH = 220;
+const SIDEBAR_MAX_WIDTH = 820;
+const SIDEBAR_START_WIDTH = 320;
 
 export const DefaultLayout = ({
 	header,
@@ -10,17 +16,49 @@ export const DefaultLayout = ({
 	header?: React.ReactNode;
 	children: React.ReactNode;
 }) => {
+	const resizer = useRef<HTMLDivElement>(null);
+	const [flexBasis, setFlexBasis] = useState(SIDEBAR_START_WIDTH);
+
+	const resize = (e: { x: number }) => {
+		const size = e.x;
+		if (size > SIDEBAR_MIN_WIDTH && size < SIDEBAR_MAX_WIDTH) setFlexBasis(e.x);
+	};
+
+	useEffect(() => {
+		const removeListener = () => {
+			document.removeEventListener('mousemove', resize, false);
+		};
+
+		const trackMovement = (event: any) => {
+			if (resizer.current && resizer.current?.contains(event?.target as HTMLDivElement)) {
+				document.addEventListener('mousemove', resize, false);
+				document.addEventListener('mouseup', removeListener, false);
+			}
+		};
+
+		document.addEventListener('mousedown', trackMovement);
+		return () => {
+			document.removeEventListener('mousedown', trackMovement);
+			document.removeEventListener('mouseup', removeListener);
+		};
+	}, []);
+
 	return (
 		<RequireSchema>
-			<div className={styles.container}>
-				<header>
-					<Header>{header}</Header>
-				</header>
-				<nav>
-					<SideBar />
-				</nav>
-				<div className={styles.content}>{children}</div>
-				{/** @todo <footer className={styles.footer}></footer> */}
+			<div className={styles.wrapper}>
+				<div className={styles.container}>
+					<div className={styles.sidebar} style={{ flexBasis: `${flexBasis}px` }}>
+						<SideBar />
+					</div>
+					<div className={styles.resizer} ref={resizer}></div>
+					<div className={styles.content}>
+						<header>
+							<Header>{header}</Header>
+						</header>
+						{children}
+					</div>
+					{/** @todo <footer className={styles.footer}></footer> */}
+				</div>
 			</div>
 		</RequireSchema>
 	);
