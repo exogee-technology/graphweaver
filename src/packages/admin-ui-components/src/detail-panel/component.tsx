4d1759f0bc2cc2bf5c6fe62c775c71beb814abcd
Steve Keep <stephen.keep@techin.site>
2023-04-20 12:35:04 +1000

    update to add new link

--- src/packages/admin-ui-components/src/detail-panel/component.tsx
+++ src/packages/admin-ui-components/src/detail-panel/component.tsx
@@ -145,7 +145,7 @@ export const DetailPanel = () => {
 		queryForEntity(selectedEntity, entityByName),
 		{
 			variables: { id },
-			skip: id === 'new',
+			skip: id === 'graphweaver-admin-new-entity',
 		}
 	);
 
