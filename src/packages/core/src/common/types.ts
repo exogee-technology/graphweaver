f8f765ab959b774549b42922fabfcb7e18e84ff9
Steve Keep <stephen.keep@techin.site>
2023-04-28 18:52:01 +1000

    initial pass at create recursion

--- src/packages/core/src/common/types.ts
+++ src/packages/core/src/common/types.ts
@@ -122,7 +122,6 @@ export interface GraphqlEntityType<G, D> {
 	name: string; // note this is the built-in ES6 class.name attribute
 	typeName?: string;
 	fromBackendEntity?(entity: D): G | null;
-	mapInputForInsertOrUpdate?(entity: Partial<G>): Partial<G>;
 }
 
 export enum AdminUIFilterType {
