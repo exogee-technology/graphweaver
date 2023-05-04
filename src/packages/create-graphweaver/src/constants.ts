4fd5630e0578ef9d54aba291f0aee519205acb8a
Steve Keep <stephen.keep@techin.site>
2023-05-05 09:43:21 +1000

    update with as-integrations

--- src/packages/create-graphweaver/src/constants.ts
+++ src/packages/create-graphweaver/src/constants.ts
@@ -2,3 +2,4 @@ import { version } from '../package.json';
 
 export const GRAPHWEAVER_TARGET_VERSION = version;
 export const MIKRO_ORM_TARGET_VERSION = '5.4.2';
+export const AWS_LAMBDA_VERSION = '2.0.1';
