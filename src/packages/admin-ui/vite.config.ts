868961c8a80bdebef8399997111cb265c4159603
Steve Keep <stephen.keep@techin.site>
2023-05-04 13:39:14 +1000

    implement optimize deps

--- src/packages/admin-ui/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [svgr(), react()],
-	server: { port: 8000 },
-});
