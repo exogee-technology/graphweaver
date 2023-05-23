fdd28df43eaf11a90e8f2dcdae8d2f47d9f08c0f
Steve Keep <stephen.keep@techin.site>
2023-05-23 16:48:18 +1000

    redirect when a guest on forbidden

--- src/packages/auth/src/authentication/providers/local/plugin.ts
+++ src/packages/auth/src/authentication/providers/local/plugin.ts
@@ -4,14 +4,17 @@ import { logger } from '@exogee/logger';
 import { AuthorizationContext } from '../../../types';
 import { LocalAuthTokenProvider } from './provider';
 import { upsertAuthorizationContext } from '../../../helper-functions';
-import { LocalAuthResolver } from './resolver';
 import { UserProfile } from '../../user-profile';
+import { ForbiddenError } from 'apollo-server-errors';
 
 if (!process.env.LOCAL_AUTH_REDIRECT_URI)
 	throw new Error('LOCAL_AUTH_REDIRECT_URI is required in environment');
 
 const redirectUrl = process.env.LOCAL_AUTH_REDIRECT_URI;
-const guestOperations = ['login'];
+
+const didEncounterForbiddenError = (error: any) => {
+	return error.extensions.code === 'FORBIDDEN';
+};
 
 export const localAuthApolloPlugin = (
 	getUserProfile: (userId: string) => Promise<UserProfile>
@@ -21,24 +24,21 @@ export const localAuthApolloPlugin = (
 		// We have two use cases this needs to handle:
 		// 1. No auth header, initial request for a guest operation.
 		//    - Some requests are allowed when accessed by a guest defined above.
-		// 2. No auth header, initial request.
-		//    - In this situation we need to redirect them to the login page.
-		// 3. There is an auth header
+		// 2. There is an auth header
 		//    - In this situation we need to verify the token, error if it's not valid and redirect.
 		//
 
 		// We may need to return a redirect to the client. If so, we'll set this variable.
 		const authHeader = request.http?.headers.get('authorization');
-		const operation = request.operationName;
-		let redirect: string;
 
 		// Case 1. No auth header, initial request for a guest operation.
-		if (guestOperations.some((guestOperation) => guestOperation === operation)) {
-			// If we are here then we do nothing as the operation requested was a guest operation
-		} else if (!authHeader) {
+		if (!authHeader) {
 			// Case 2. No auth header, initial request.
 			logger.trace('No Auth header, setting redirect');
-			redirect = redirectUrl;
+
+			// We are a guest and have not logged in yet.
+			contextValue.roles = ['GUEST'];
+			upsertAuthorizationContext(contextValue);
 		} else {
 			// Case 3. There is an auth header
 			logger.trace('Got a token, checking it is valid.');
@@ -56,16 +56,26 @@ export const localAuthApolloPlugin = (
 
 				upsertAuthorizationContext(contextValue);
 			} catch {
-				redirect = redirectUrl;
+				throw new ForbiddenError('Forbidden: Authentication Failed');
 			}
 		}
 
 		return {
-			willSendResponse: async ({ response }) => {
-				// If we need to redirect, set the header to tell the client to do so.
-				if (redirect) {
-					logger.trace('Redirect set, setting X-Auth-Redirect header.');
-					response.http?.headers.set('X-Auth-Redirect', redirect);
+			willSendResponse: async ({ response, contextValue }) => {
+				// Let's check if we are a guest and have received a forbidden error
+				if (
+					contextValue.roles?.includes('GUEST') &&
+					response &&
+					(response.body as any)?.singleResult?.errors
+				) {
+					const didEncounterForbiddenErrors = (response.body as any)?.singleResult?.errors.some(
+						didEncounterForbiddenError
+					);
+					//If we received a forbidden error we need to redirect, set the header to tell the client to do so.
+					if (didEncounterForbiddenErrors) {
+						logger.trace('Forbidden Error Found: setting X-Auth-Redirect header.');
+						response.http?.headers.set('X-Auth-Redirect', redirectUrl);
+					}
 				}
 			},
 		};
