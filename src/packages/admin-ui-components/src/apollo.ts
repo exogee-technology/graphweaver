8d39de24802731d557cb9c7dc4fb1b3ce30542f7
Kye <kye.lewis@techin.site>
2023-05-25 10:16:48 +1000

    Update CORS configuration

--- src/packages/admin-ui-components/src/apollo.ts
+++ src/packages/admin-ui-components/src/apollo.ts
@@ -3,36 +3,45 @@ import { uri } from './config';
 
 const httpLink = new HttpLink({
 	uri,
-	credentials: 'include',
 });
 
-// This link does several things:
-//   1. If there's something called `graphweaver-auth` in local storage, we need to send that to the server.
-//   2. If the server sends back a header called `X-Auth-Redirect` on any response, we need to redirect the user to that URL.
-//   3. If the server sends back a header called `Authorization` on any response, we need to update our `graphweaver-auth` local storage value with
-//      what we got from the server.
 const authLink = new ApolloLink((operation, forward) => {
-	// Feature 1 above ☝️
-	if (localStorage.getItem('graphweaver-auth')) {
-		operation.setContext({
-			headers: {
-				Authorization: localStorage.getItem('graphweaver-auth'),
-			},
-		});
-	}
+	//  If there's something called `graphweaver-auth` in local storage, we need to send that to the server.
+	const currentAuthToken = localStorage.getItem('graphweaver-auth');
+
+	// The token should include the type and the credential, if not let's emit a warning.
+	if (currentAuthToken && currentAuthToken.split(' ').length < 2)
+		console.warn(
+			'Current Graphweaver Auth Token is invalid, it should be in the form "[type] [credential]"'
+		);
+
+	operation.setContext({
+		headers: {
+			'Apollo-Require-Preflight': 'true',
+			'Content-Type': 'application/json',
+			...(currentAuthToken ? { Authorization: currentAuthToken } : {}),
+		},
+	});
 
 	return forward(operation).map((response) => {
 		const context = operation.getContext();
 
-		// Feature 2 above ☝️
-		// Do we have an auth redirect we need to do for OAuth?
+		//  If the server sends back a header called `X-Auth-Redirect` on any response, we need to redirect the user to that URL.
 		const redirectHeader = context.response.headers.get('X-Auth-Redirect');
 		if (redirectHeader) window.location.href = redirectHeader;
 
-		// Feature 3 above ☝️
-		// Do we have an auth header we need to store?
-		const authHeader = context.response.headers.get('Authorization');
-		if (authHeader) localStorage.setItem('graphweaver-auth', authHeader);
+		// If the server sends back a header called `Authorization` on any response, we need to
+		// update our `graphweaver-auth` local storage value with what we got from the server.
+		const newAuthToken = context.response.headers.get('Authorization');
+
+		if (newAuthToken) {
+			// The token should include the type and the credential, if not let's emit a warning.
+			if (newAuthToken.split(' ').length < 2)
+				console.warn(
+					'New Graphweaver Auth Token is invalid, it should be in the form "[type] [credential]"'
+				);
+			localStorage.setItem('graphweaver-auth', newAuthToken);
+		}
 
 		return response;
 	});
