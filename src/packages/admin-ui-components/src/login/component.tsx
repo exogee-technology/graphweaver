d689ac47129e1d2d00d4f9fb8bb9450a0652d2bb
Kye <kye.lewis@techin.site>
2023-06-08 11:46:03 +1000

    Add custom login props, fix auth env error, bump to 0.1.22

--- src/packages/admin-ui-components/src/login/component.tsx
+++ src/packages/admin-ui-components/src/login/component.tsx
@@ -1,51 +1,72 @@
 import { Field, Form, Formik, FormikHelpers } from 'formik';
 import { GraphweaverLogo } from '../assets';
 import { Button } from '../button';
-import { Input } from '../input';
 
 import styles from './styles.module.css';
 import { useMutation } from '@apollo/client';
 
 import { LOGIN_MUTATION } from './graphql';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
-export const Login = () => {
+interface Form {
+	username: string;
+	password: string;
+}
+
+export interface LoginProps {
+	onLogin?(username: string, password: string): string; // Returns a token to place in Authorization header
+}
+
+export const Login = ({ onLogin }: LoginProps) => {
 	const navigate = useNavigate();
 	const [login] = useMutation<{ login: { authToken: string } }>(LOGIN_MUTATION);
+	const [error, setError] = useState<Error | undefined>();
 
-	const handleOnSubmit = async (values: any, actions: FormikHelpers<any>) => {
-		const res = await login({
-			variables: {
-				username: values.username,
-				password: values.password,
-			},
-		});
+	const handleOnSubmit = async (values: Form, { setSubmitting }: FormikHelpers<Form>) => {
+		let token;
 
-		actions.setSubmitting(false);
+		try {
+			if (onLogin) {
+				token = onLogin(values.username, values.password);
+			} else {
+				const { data } = await login({
+					variables: {
+						username: values.username,
+						password: values.password,
+					},
+				});
 
-		if (res?.data?.login.authToken) {
-			localStorage.setItem('graphweaver-auth', res.data.login.authToken);
-			navigate('/');
+				token = data?.login.authToken;
+				if (!token) throw new Error('Missing token');
+
+				localStorage.setItem('graphweaver-auth', token);
+				navigate('/');
+			}
+		} catch (error) {
+			setError(error instanceof Error ? error : new Error(String(error)));
+		} finally {
+			setSubmitting(false);
 		}
 	};
 
 	return (
-		<Formik initialValues={{ username: '', password: '' }} onSubmit={handleOnSubmit}>
+		<Formik<Form> initialValues={{ username: '', password: '' }} onSubmit={handleOnSubmit}>
 			{({ isSubmitting }) => (
 				<Form className={styles.wrapper}>
 					<GraphweaverLogo width="52" className={styles.logo} />
 					<div className={styles.titleContainer}>Login</div>
 					<Field
-						placeholder={'Username'}
-						id={'username'}
-						name={'username'}
+						placeholder="Username"
+						id="username"
+						name="username"
 						className={styles.textInputField}
 					/>
 					<Field
-						type={'password'}
-						placeholder={'Password'}
-						id={'password'}
-						name={'password'}
+						type="password"
+						placeholder="Password"
+						id="password"
+						name="password"
 						className={styles.textInputField}
 					/>
 					<div className={styles.buttonContainer}>
@@ -53,6 +74,11 @@ export const Login = () => {
 							Login
 						</Button>
 					</div>
+					{!!error && (
+						<div>
+							<b>Error: ${error.message}</b>
+						</div>
+					)}
 				</Form>
 			)}
 		</Formik>
