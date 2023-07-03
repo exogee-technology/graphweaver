bf4a589a83e55a6bf7749f9fad1f8e129b57342f
Taylor Nodell <taylor.nodell@techin.site>
2023-07-03 14:57:35 +1000

    Removes loggers in Xero example

--- src/examples/xero/src/backend/index.ts
+++ src/examples/xero/src/backend/index.ts
@@ -1,19 +1,16 @@
 import 'reflect-metadata';
 import Graphweaver from '@exogee/graphweaver-apollo';
-import { logger } from '@exogee/logger';
 import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
 
 import { AccountResolver, ProfitAndLossRowResolver, TenantResolver } from './schema';
 import { XeroAuthApolloPlugin } from '@exogee/graphweaver-xero';
 
-logger.info(`example-xero start Graphweaver`);
 const graphweaver = new Graphweaver({
 	resolvers: [AccountResolver, ProfitAndLossRowResolver, TenantResolver],
 	apolloServerOptions: {
 		plugins: [XeroAuthApolloPlugin],
 	},
 });
-logger.info(`example-xero graphweaver.server start`);
 
 exports.handler = startServerAndCreateLambdaHandler(
 	graphweaver.server,
