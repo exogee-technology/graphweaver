534bb7a1d94570d6ecc557bbba136f662dacd5d6
Steve Keep <stephen.keep@techin.site>
2023-07-03 11:52:44 +1000

    create example import project

--- src/packages/cli/src/import/index.ts
+++ src/packages/cli/src/import/index.ts
@@ -28,8 +28,8 @@ export const importDataSource = async () => {
 	console.log('Import complete.');
 
 	for (const file of files) {
-		createDirectories(path.join('./src/dist/', file.path));
-		writeFileSync(path.join(process.cwd(), './src/dist/', file.path, file.name), file.contents);
+		createDirectories(path.join('./src/', file.path));
+		writeFileSync(path.join(process.cwd(), './src/', file.path, file.name), file.contents);
 	}
 
 	// Force exit because Mikro is keeping the socket open to the db
