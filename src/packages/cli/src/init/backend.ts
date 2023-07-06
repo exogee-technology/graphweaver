028088f1667bbd0e25fe5acd21cbfbce5afce543
Steve Keep <stephen.keep@techin.site>
2023-07-06 11:53:33 +1000

    fix graphweaver versions

--- src/packages/cli/src/init/backend.ts
+++ src/packages/cli/src/init/backend.ts
@@ -6,27 +6,27 @@ export enum Backend {
 	REST,
 }
 
-export const packagesForBackend = (backend: Backend): Record<string, string> => {
+export const packagesForBackend = (backend: Backend, version?: string): Record<string, string> => {
+	const graphWeaverVersion = version ?? GRAPHWEAVER_TARGET_VERSION;
+
 	switch (backend) {
 		case Backend.MikroOrmPostgres:
 			return {
-				'@exogee/graphweaver-mikroorm': GRAPHWEAVER_TARGET_VERSION,
-				'@exogee/graphweaver-apollo': GRAPHWEAVER_TARGET_VERSION,
+				'@exogee/graphweaver-mikroorm': graphWeaverVersion,
 				'@mikro-orm/core': MIKRO_ORM_TARGET_VERSION,
 				'@mikro-orm/postgresql': MIKRO_ORM_TARGET_VERSION,
 			};
 
 		case Backend.MikroOrmMysql:
 			return {
-				'@exogee/graphweaver-mikroorm': GRAPHWEAVER_TARGET_VERSION,
-				'@exogee/graphweaver-apollo': GRAPHWEAVER_TARGET_VERSION,
+				'@exogee/graphweaver-mikroorm': graphWeaverVersion,
 				'@mikro-orm/core': MIKRO_ORM_TARGET_VERSION,
 				'@mikro-orm/mysql': MIKRO_ORM_TARGET_VERSION,
 			};
 
 		case Backend.REST:
 			return {
-				'@exogee/graphweaver-rest': GRAPHWEAVER_TARGET_VERSION,
+				'@exogee/graphweaver-rest': graphWeaverVersion,
 			};
 	}
 };
