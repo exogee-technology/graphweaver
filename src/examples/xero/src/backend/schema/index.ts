670fdca2b7dbb3b12f7467442ad012f6adc1df9e
Taylor Nodell <taylor.nodell@techin.site>
2023-07-17 16:01:07 +1000

    Removes function typing

--- src/examples/xero/src/backend/schema/index.ts
+++ src/examples/xero/src/backend/schema/index.ts
@@ -2,10 +2,4 @@ import { AccountResolver } from './AccountResolver';
 import { ProfitAndLossRowResolver } from './ProfitAndLossRowResolver';
 import { TenantResolver } from './TenantResolver';
 
-// The Function type is the type that Type GraphQL expects
-// eslint-disable-next-line @typescript-eslint/ban-types
-export const resolvers: [Function, ...Function[]] = [
-	AccountResolver,
-	ProfitAndLossRowResolver,
-	TenantResolver,
-];
+export const resolvers = [AccountResolver, ProfitAndLossRowResolver, TenantResolver];
