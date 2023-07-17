4b34c3173fcf38888b2868c9c74a247c865d7361
Taylor Nodell <taylor.nodell@techin.site>
2023-07-17 13:31:28 +1000

    Removes Function typing

--- src/examples/databases/src/backend/schema/index.ts
+++ src/examples/databases/src/backend/schema/index.ts
@@ -1,6 +1,4 @@
 import { UserResolver } from './user';
 import { TaskResolver } from './task';
 
-// The Function type is the type that Type GraphQL expects
-// eslint-disable-next-line @typescript-eslint/ban-types
-export const resolvers: [Function, ...Function[]] = [UserResolver, TaskResolver];
+export const resolvers = [UserResolver, TaskResolver];
