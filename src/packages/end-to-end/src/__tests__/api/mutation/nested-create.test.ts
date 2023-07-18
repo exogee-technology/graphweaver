7b2749dbeb1708c53475294f618713623a5f2444
Steve Keep <stephen.keep@techin.site>
2023-07-19 09:05:01 +1000

    create inverse failing test

--- src/packages/end-to-end/src/__tests__/api/mutation/nested-create.test.ts
+++ src/packages/end-to-end/src/__tests__/api/mutation/nested-create.test.ts
@@ -1,11 +1,11 @@
 import request from 'supertest-graphql';
 import gql from 'graphql-tag';
 
-import { Album } from '../../../types';
+import { Album, Artist } from '../../../types';
 import { config } from '../../../config';
 import { resetDatabase } from '../../../utils';
 
-describe('basic mutation', () => {
+describe('nested create', () => {
 	beforeEach(resetDatabase);
 
 	test('should create an album and an artist', async () => {
@@ -29,5 +29,28 @@ describe('basic mutation', () => {
 		expect(data?.createAlbum?.id).toBe('348');
 		expect(data?.createAlbum?.ArtistId?.id).toBe('276');
 		expect(data?.createAlbum?.ArtistId?.Name).toBe('string');
+	});
+
+	test('should create an artist and an album', async () => {
+		const { data } = await request<{ createAlbum: Artist }>(config.baseUrl)
+			.mutate(
+				gql`
+					mutation CreateArtist($data: ArtistInsertInput!) {
+						createArtist(data: $data) {
+							id
+							ArtistIdInverse {
+								id
+								Title
+							}
+						}
+					}
+				`
+			)
+			.variables({ data: { ArtistIdInverse: [{ Title: 'string' }], Name: 'string' } })
+			.expectNoErrors();
+
+		expect(data?.createAlbum?.id).toBe('348');
+		expect(data?.createAlbum?.ArtistIdInverse?.[0]?.id).toBe('276');
+		expect(data?.createAlbum?.ArtistIdInverse?.[0]?.Title).toBe('string');
 	});
 });
