3c52330cb5e378bd8827619d72ed713599dedc21
Taylor Nodell <taylor.nodell@techin.site>
2023-07-18 14:40:58 +1000

    Addresses PR comments

--- src/examples/databases/src/backend/index.ts
+++ src/examples/databases/src/backend/index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import Graphweaver from '@exogee/graphweaver-apollo';
-import { ClearDatabaseContext, connectToDatabase } from '@exogee/graphweaver-mikroorm';
+import { connectToDatabase } from '@exogee/graphweaver-mikroorm';
 
 import { resolvers } from './schema';
 
@@ -9,7 +9,7 @@ import { connections } from './database';
 const graphweaver = new Graphweaver({
 	resolvers: resolvers,
 	apolloServerOptions: {
-		plugins: [connectToDatabase(connections), ClearDatabaseContext],
+		plugins: [connectToDatabase(connections)],
 	},
 });
 
