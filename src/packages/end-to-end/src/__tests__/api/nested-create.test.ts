65b1709dc670f40685cfb5b528a1410fb1932afe
Steve Keep <stephen.keep@techin.site>
2023-07-18 17:50:09 +1000

    created failing nested test

--- src/packages/end-to-end/src/__tests__/api/nested-create.test.ts
+++ src/packages/end-to-end/src/__tests__/api/nested-create.test.ts
@@ -1,27 +1,33 @@
 import request from 'supertest-graphql';
 import gql from 'graphql-tag';
 
-import { Album } from '../types';
-import { config } from '../config';
-import { resetDatabase } from '../utils';
+import { Album } from '../../types';
+import { config } from '../../config';
+import { resetDatabase } from '../../utils';
 
 describe('basic mutation', () => {
 	beforeEach(resetDatabase);
 
-	test('should create an album', async () => {
+	test('should create an album and an artist', async () => {
 		const { data } = await request<{ createAlbum: Album }>(config.baseUrl)
 			.mutate(
 				gql`
 					mutation CreateAlbum($data: AlbumInsertInput!) {
 						createAlbum(data: $data) {
 							id
+							ArtistId {
+								id
+								Name
+							}
 						}
 					}
 				`
 			)
-			.variables({ data: { ArtistId: { id: 1 }, Title: 'string' } })
+			.variables({ data: { ArtistId: { Name: 'string' }, Title: 'string' } })
 			.expectNoErrors();
 
 		expect(data?.createAlbum?.id).toBe('348');
+		expect(data?.createAlbum?.ArtistId?.id).toBe('276');
+		expect(data?.createAlbum?.ArtistId?.Name).toBe('string');
 	});
 });
