3c4ff9bcfe0ef44289695eb638e9b5901bc1052f
Steve Keep <stephen.keep@techin.site>
2023-07-20 15:25:00 +1000

    check if the jwt is expired before sending to the server

--- src/packages/admin-ui-components/src/apollo.ts
+++ src/packages/admin-ui-components/src/apollo.ts
@@ -3,19 +3,36 @@ import { inflate } from 'graphql-deduplicator';
 
 import { uri } from './config';
 
+const AUTH_TOKEN_LOCAL_STORAGE_KEY = 'graphweaver-auth';
+
 const httpLink = new HttpLink({
 	uri,
 });
 
+const isExpired = (token: string) => {
+	try {
+		const decodedJwt = JSON.parse(atob(token.split('.')[1]));
+		return decodedJwt.exp * 1000 < Date.now();
+	} catch (e) {
+		return true;
+	}
+};
+
 const authLink = new ApolloLink((operation, forward) => {
 	//  If there's something called `graphweaver-auth` in local storage, we need to send that to the server.
-	const currentAuthToken = localStorage.getItem('graphweaver-auth');
+	let currentAuthToken = localStorage.getItem(AUTH_TOKEN_LOCAL_STORAGE_KEY);
 
 	// The token should include the type and the credential, if not let's emit a warning.
 	if (currentAuthToken && currentAuthToken.split(' ').length < 2)
 		console.warn(
 			'Current Graphweaver Auth Token is invalid, it should be in the form "[type] [credential]"'
 		);
+
+	if (currentAuthToken && isExpired(currentAuthToken)) {
+		console.warn('Current Graphweaver Auth Token is expired, removing expired token.');
+		localStorage.removeItem(AUTH_TOKEN_LOCAL_STORAGE_KEY);
+		currentAuthToken = null;
+	}
 
 	operation.setContext({
 		headers: {
@@ -42,7 +59,7 @@ const authLink = new ApolloLink((operation, forward) => {
 				console.warn(
 					'New Graphweaver Auth Token is invalid, it should be in the form "[type] [credential]"'
 				);
-			localStorage.setItem('graphweaver-auth', newAuthToken);
+			localStorage.setItem(AUTH_TOKEN_LOCAL_STORAGE_KEY, newAuthToken);
 		}
 
 		// Inflate the response data, this is deduplicated by default
