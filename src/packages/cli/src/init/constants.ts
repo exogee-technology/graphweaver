6fffa03b5a75e6f457d5f3993dca484c6b271842
Steve Keep <stephen.keep@techin.site>
2023-07-24 11:23:21 +1000

    allow local keyword to link

--- src/packages/cli/src/init/constants.ts
+++ src/packages/cli/src/init/constants.ts
@@ -3,3 +3,10 @@ import { version } from '../../package.json';
 export const GRAPHWEAVER_TARGET_VERSION = version;
 export const MIKRO_ORM_TARGET_VERSION = '5.4.2';
 export const AWS_LAMBDA_VERSION = '2.0.1';
+
+export const graphweaverVersion = (versionOverride?: string, packageName?: string) => {
+	if (versionOverride === 'local' && packageName) {
+		return `link:../../${packageName}`;
+	}
+	return versionOverride ? versionOverride : GRAPHWEAVER_TARGET_VERSION;
+};
