1ce85f89fa745b1cefea25a0e77d7c3c3b0d9948
Taylor Nodell <taylor.nodell@techin.site>
2023-07-25 15:11:20 +1000

    Cleans up example code

--- src/examples/databases/src/backend/index.ts
+++ src/examples/databases/src/backend/index.ts
@@ -1,15 +1,10 @@
 import 'reflect-metadata';
 import Graphweaver from '@exogee/graphweaver-apollo';
-import { connectToDatabase } from '@exogee/graphweaver-mikroorm';
 
 import { resolvers } from './schema';
-import { connections } from './database';
 
 const graphweaver = new Graphweaver({
 	resolvers,
-	apolloServerOptions: {
-		plugins: [connectToDatabase(connections)],
-	},
 });
 
 exports.handler = graphweaver.handler();
