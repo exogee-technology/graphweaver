17065826c0a9b8c9bb0729a79320a61b01bf6417
Steve Keep <stephen.keep@techin.site>
2023-07-26 16:07:32 +1000

    fix too many clients already error

--- src/packages/mikroorm/src/plugins/connect-to-database.ts
+++ src/packages/mikroorm/src/plugins/connect-to-database.ts
@@ -1,6 +1,8 @@
 import { ApolloServerPlugin } from '@apollo/server';
 import { ConnectionManager, ConnectionOptions } from '../database';
 
+const connectionIds = new Set<string>();
+
 export const connectToDatabase = (
 	options: ConnectionOptions[] | ConnectionOptions
 ): ApolloServerPlugin => {
@@ -8,11 +10,14 @@ export const connectToDatabase = (
 		serverWillStart: async () => {
 			if (Array.isArray(options)) {
 				for (const option of options) {
-					if (option.connectionManagerId)
+					if (option.connectionManagerId && !connectionIds.has(option.connectionManagerId)) {
+						connectionIds.add(option.connectionManagerId);
 						await ConnectionManager.connect(option.connectionManagerId, option);
+					}
 				}
 			} else {
-				if (options.connectionManagerId) {
+				if (options.connectionManagerId && !connectionIds.has(options.connectionManagerId)) {
+					connectionIds.add(options.connectionManagerId);
 					await ConnectionManager.connect(options.connectionManagerId, options);
 				}
 			}
