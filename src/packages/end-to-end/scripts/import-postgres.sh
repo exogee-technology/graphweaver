6881c310304d6d6213f66768fc3893012dd69e78
Taylor Nodell <taylor.nodell@techin.site>
2023-07-27 16:05:00 +1000

    Updates postgres script

--- src/packages/end-to-end/scripts/import-postgres.sh
+++ src/packages/end-to-end/scripts/import-postgres.sh
@@ -1,1 +1,9 @@
-cp ./databases/database.sqlite ./app/databases/database.sqlite && cd app && node ../../cli/bin import sql --database=database.sql
+createdb gw
+psql -U postgres -d gw -c "CREATE TABLE \"user\" (id BIGSERIAL PRIMARY KEY, username VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255), last_name VARCHAR(255));"
+psql -U postgres -d gw -c "
+  INSERT INTO \"user\" (username, password, first_name, last_name) VALUES
+    ('user1', 'password1', 'John', 'Doe'),
+    ('user2', 'password2', 'Jane', 'Smith'),
+    ('user3', 'password3', 'Alice', 'Johnson'),
+    ('user4', 'password4', 'Bob', 'Brown');
+"
