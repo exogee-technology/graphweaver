a61657712c3fab7a3929e28401c2eade63952fed
Taylor Nodell <taylor.nodell@techin.site>
2023-07-27 15:37:40 +1000

    Gets app databases directory working

--- src/packages/end-to-end/scripts/import-sqlite.sh
+++ src/packages/end-to-end/scripts/import-sqlite.sh
@@ -1,1 +1,4 @@
-cp ./databases/database.sqlite ./app/databases/database.sqlite && cd app && node ../../cli/bin import sqlite --database=database.sqlite
+mkdir app/databases
+cp ./databases/database.sqlite ./app/databases/database.sqlite 
+cd app 
+node ../../cli/bin import sqlite --database=databases/database.sqlite
