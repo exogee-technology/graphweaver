f5d260991caba14c58ecfc890bd8fdb4449debed
Taylor Nodell <taylor.nodell@techin.site>
2023-07-28 10:53:04 +1000

    Adds setup-postgres

--- src/packages/end-to-end/scripts/setup-postgres.sh
+++ src/packages/end-to-end/scripts/setup-postgres.sh
@@ -1,4 +1,9 @@
-# createdb gw
+createdb gw
+# Wait for PostgreSQL to start
+until psql -h localhost -U postgres -d gw -c "SELECT 1" &> /dev/null; do
+    sleep 1
+done
+
 psql -U postgres -d gw -c "CREATE TABLE \"user\" (id BIGSERIAL PRIMARY KEY, username VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255), last_name VARCHAR(255));"
 psql -U postgres -d gw -c "
   INSERT INTO \"user\" (username, password, first_name, last_name) VALUES
