b7cd3ebf358a0cbdd90cda253ba4cdb0afcb73ea
Taylor Nodell <taylor.nodell@techin.site>
2023-07-31 10:18:49 +1000

    wraps in an async func

--- src/packages/end-to-end/scripts/client.js
+++ src/packages/end-to-end/scripts/client.js
@@ -2,37 +2,43 @@
 const { Client } = require('pg');
 
 const pgclient = new Client({
-	host: 'postgres',
+	host: process.env.POSTGRES_HOST, // Use the environment variable for the hostname
 	port: process.env.POSTGRES_PORT,
 	user: 'postgres',
 	password: 'postgres',
-	database: 'postgres',
+	database: 'gw', // Use the correct database name here
 });
 
-pgclient.connect();
+async function connectAndQuery() {
+	try {
+		await pgclient.connect();
+		console.log('Connected to PostgreSQL');
 
-const table =
-	'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))';
-const text =
-	'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *';
-const values = [
-	'Mona the',
-	'Octocat',
-	9,
-	'88 Colin P Kelly Jr St, San Francisco, CA 94107, United States',
-	'octocat@github.com',
-];
+		const table =
+			'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))';
+		const text =
+			'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *';
+		const values = [
+			'Mona the',
+			'Octocat',
+			9,
+			'88 Colin P Kelly Jr St, San Francisco, CA 94107, United States',
+			'octocat@github.com',
+		];
 
-pgclient.query(table, (err, res) => {
-	if (err) throw err;
-});
+		await pgclient.query(table);
+		const result = await pgclient.query(text, values);
+		console.log('Inserted data:', result.rows[0]);
 
-pgclient.query(text, values, (err, res) => {
-	if (err) throw err;
-});
+		const queryResult = await pgclient.query('SELECT * FROM student');
+		console.log('Data in student table:', queryResult.rows);
 
-pgclient.query('SELECT * FROM student', (err, res) => {
-	if (err) throw err;
-	console.log(err, res.rows); // Print the data in student table
-	pgclient.end();
-});
+		await pgclient.end();
+		console.log('Connection closed');
+	} catch (error) {
+		console.error('Error:', error);
+		process.exit(1); // Exit with a non-zero code to indicate an error occurred
+	}
+}
+
+await connectAndQuery();
