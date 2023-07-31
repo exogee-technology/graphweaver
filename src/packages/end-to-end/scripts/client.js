7ce79aa8c72f0caaa82a00b21e69773e3566ae47
Taylor Nodell <taylor.nodell@techin.site>
2023-07-31 10:11:52 +1000

    Changes hostname

--- src/packages/end-to-end/scripts/client.js
+++ src/packages/end-to-end/scripts/client.js
@@ -2,7 +2,7 @@
 const { Client } = require('pg');
 
 const pgclient = new Client({
-	host: process.env.POSTGRES_HOST,
+	host: 'postgres',
 	port: process.env.POSTGRES_PORT,
 	user: 'postgres',
 	password: 'postgres',
