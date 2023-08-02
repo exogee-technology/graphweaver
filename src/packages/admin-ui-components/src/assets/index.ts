0f499762f63bf58f061a0d75781b8c17d1b7a0ff
Siow <siow.koon@exogee.com>
2023-08-02 12:08:31 +1000

    added icons

--- src/packages/admin-ui-components/src/assets/index.ts
+++ src/packages/admin-ui-components/src/assets/index.ts
@@ -10,3 +10,7 @@ export { ReactComponent as DatabaseIcon24 } from './24-database.svg';
 export { ReactComponent as DataSourcesIcon } from './64-data-sources.svg';
 export { ReactComponent as GraphweaverLogo } from './graphweaver-logo.svg';
 export { ReactComponent as ExitIcon } from './close-button-svgrepo-com.svg';
+export { ReactComponent as SuccessIcon } from './16-success.svg';
+export { ReactComponent as ErrorIcon } from './16-error.svg';
+export { ReactComponent as InfoIcon } from './16-info.svg';
+export { ReactComponent as WarningIcon } from './16-warning.svg';
