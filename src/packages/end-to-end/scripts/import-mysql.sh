e84b157598db3e388879a564862e5de305bbc39f
Siow <siow.koon@exogee.com>
2023-08-08 13:08:02 +1000

    updated db host

--- src/packages/end-to-end/scripts/import-mysql.sh
+++ src/packages/end-to-end/scripts/import-mysql.sh
@@ -4,10 +4,7 @@ cd app
 pnpm i --ignore-workspace --no-lockfile
 mkdir databases
 cp ../databases/mysql.sql databases/mysql.sql 
-mysql -hlocalhost -u $MYSQL_USER --password=$MYSQL_PASSWORD -e "DROP DATABASE IF EXISTS Chinook;"
-mysql -hlocalhost -u $MYSQL_USER --password=$MYSQL_PASSWORD -e "CREATE DATABASE Chinook;"
-mysql -hlocalhost -u $MYSQL_USER --password=$MYSQL_PASSWORD Chinook < databases/mysql.sql
-mysql -hlocalhost -u $MYSQL_USER --password=$MYSQL_PASSWORD -e "CREATE USER IF NOT EXISTS 'tester'@'localhost' IDENTIFIED BY 'password';"
-mysql -hlocalhost -u $MYSQL_USER --password=$MYSQL_PASSWORD -e "GRANT ALL ON Chinook.* TO 'tester'@'localhost';"
-mysql -hlocalhost -u $MYSQL_USER --password=$MYSQL_PASSWORD -e "FLUSH PRIVILEGES;"
-node ../../cli/bin import mysql --database=Chinook --user=tester --password=password --host="127.0.0.1" --port=3306 
+mysql -h$DATABASE_HOST -u$DATABASE_USERNAME --password=$DATABASE_PASSWORD -e "DROP DATABASE IF EXISTS Chinook;"
+mysql -h$DATABASE_HOST -u$DATABASE_USERNAME --password=$DATABASE_PASSWORD -e "CREATE DATABASE Chinook;"
+mysql -h$DATABASE_HOST -u$DATABASE_USERNAME --password=$DATABASE_PASSWORD Chinook < databases/mysql.sql
+node ../../cli/bin import mysql --database=Chinook --user=$DATABASE_USERNAME --password=$DATABASE_PASSWORD --host=$DATABASE_HOST --port=3306 
