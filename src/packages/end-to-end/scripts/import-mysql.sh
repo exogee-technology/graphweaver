4a642342be86f94f1f4b03fd96d0dc882c7238bb
Siow <siow.koon@exogee.com>
2023-08-08 11:58:34 +1000

    added host

--- src/packages/end-to-end/scripts/import-mysql.sh
+++ src/packages/end-to-end/scripts/import-mysql.sh
@@ -4,7 +4,7 @@ cd app
 pnpm i --ignore-workspace --no-lockfile
 mkdir databases
 cp ../databases/mysql.sql databases/mysql.sql 
-mysql -u root --password=$MYSQL_PASSWORD -e "DROP DATABASE Chinook;"
-mysql -u root --password=$MYSQL_PASSWORD -e "CREATE DATABASE Chinook;"
-mysql -u root --password=$MYSQL_PASSWORD Chinook < databases/mysql.sql
+mysql -hlocalhost -u root --password=$MYSQL_PASSWORD -e "DROP DATABASE IF EXISTS Chinook;"
+mysql -hlocalhost -u root --password=$MYSQL_PASSWORD -e "CREATE DATABASE Chinook;"
+mysql -hlocalhost -u root --password=$MYSQL_PASSWORD Chinook < databases/mysql.sql
 node ../../cli/bin import mysql --database=Chinook --user=root --password=$MYSQL_PASSWORD --host=localhost --port=3306 
