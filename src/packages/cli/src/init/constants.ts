a06a21017111e5a23fe1fe019b6723a11b32efc1
Steve Keep <stephen.keep@techin.site>
2023-08-15 16:24:47 +1000

    bump mikro

--- src/packages/cli/src/init/constants.ts
+++ src/packages/cli/src/init/constants.ts
@@ -1,7 +1,7 @@
 import { version } from '../../package.json';
 
 export const GRAPHWEAVER_TARGET_VERSION = version;
-export const MIKRO_ORM_TARGET_VERSION = '5.4.2';
+export const MIKRO_ORM_TARGET_VERSION = '5.7.14';
 export const AWS_LAMBDA_VERSION = '2.0.1';
 
 export const graphweaverVersion = (versionOverride?: string, packageName?: string) => {
