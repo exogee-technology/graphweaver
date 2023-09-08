04031159f20106b453f64685680269a94b78eef9
Steve Keep <stephen.keep@techin.site>
2023-09-08 11:53:37 +1000

    extend the BaseAuthTokenProvider interface

--- src/packages/auth/src/authentication/base-auth-token-provider.ts
+++ src/packages/auth/src/authentication/base-auth-token-provider.ts
@@ -1,9 +1,12 @@
 import { JwtPayload } from 'jsonwebtoken';
+import { AuthToken } from './schema';
+import { UserProfile } from '../user-profile';
 
 export enum AuthProvider {
 	LOCAL = 'LOCAL',
 }
 
 export interface BaseAuthTokenProvider {
+	generateToken: (user: UserProfile) => Promise<AuthToken>;
 	decodeToken: (authToken: string) => Promise<string | JwtPayload>;
 }
