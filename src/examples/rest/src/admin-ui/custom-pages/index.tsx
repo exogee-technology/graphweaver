138337888571427af637cde5b726bacb5a1c6ed4
Steve Keep <stephen.keep@techin.site>
2023-09-19 10:04:42 +1000

    move routes to package

--- src/examples/rest/src/admin-ui/custom-pages/index.tsx
+++ src/examples/rest/src/admin-ui/custom-pages/index.tsx
@@ -1,20 +1,5 @@
-import { Auth, Login, Challenge } from '@exogee/graphweaver-auth-ui-components';
+import { routes } from '@exogee/graphweaver-auth-ui-components';
 
 export const customPages = {
-	routes: () => [
-		{
-			path: '/auth',
-			element: <Auth />,
-			children: [
-				{
-					path: 'login',
-					element: <Login />,
-				},
-				{
-					path: 'challenge',
-					element: <Challenge />,
-				},
-			],
-		},
-	],
+	routes: () => [...routes.password],
 };
