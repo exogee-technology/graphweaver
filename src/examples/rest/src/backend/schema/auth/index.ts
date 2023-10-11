eb70d1a485ca7209d71a4400f874d13f7dd8bff2
Steve Keep <stephen.keep@techin.site>
2023-10-11 12:16:59 +1100

    remove unused enum

--- src/examples/rest/src/backend/schema/auth/index.ts
+++ src/examples/rest/src/backend/schema/auth/index.ts
@@ -3,11 +3,3 @@ export * from './magic-link/resolver';
 export * from './one-time-password/resolver';
 export * from './web3/resolver';
 export * from './passkey/resolver';
-
-export enum AuthenticationType {
-	PasskeyChallenge = 'PasskeyChallenge',
-	PasskeyAuthenticator = 'PasskeyAuthenticator',
-	Web3WalletAddress = 'Web3WalletAddress',
-	OneTimePasswordChallenge = 'OneTimePasswordChallenge',
-	MagicLinkChallenge = 'MagicLinkChallenge',
-}
