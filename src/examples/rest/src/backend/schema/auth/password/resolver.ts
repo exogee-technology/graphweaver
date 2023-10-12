f749f1d820d4e86cde1b90322cda62bd1a6b1e25
Steve Keep <stephen.keep@techin.site>
2023-10-12 12:30:00 +1100

    send OTP when registering

--- src/examples/rest/src/backend/schema/auth/password/resolver.ts
+++ src/examples/rest/src/backend/schema/auth/password/resolver.ts
@@ -1,6 +1,7 @@
 import {
 	PasswordAuthResolver as AuthResolver,
 	PasswordOperation,
+	RequestParams,
 	UserProfile,
 } from '@exogee/graphweaver-auth';
 import { BaseLoaders, Resolver } from '@exogee/graphweaver';
@@ -10,6 +11,7 @@ import { User } from '../../user';
 import { mapUserToProfile } from '../../../auth/context';
 import { myConnection } from '../../../database';
 import { Credential } from '../../../entities/mysql';
+import { callChildMutation } from '@exogee/graphweaver';
 
 @Resolver()
 export class PasswordAuthResolver extends AuthResolver {
@@ -19,13 +21,18 @@ export class PasswordAuthResolver extends AuthResolver {
 		});
 	}
 
-	async getUser(id: string, operation: PasswordOperation): Promise<UserProfile> {
+	async getUser(
+		id: string,
+		operation: PasswordOperation,
+		params: RequestParams
+	): Promise<UserProfile> {
 		const user = User.fromBackendEntity(await BaseLoaders.loadOne({ gqlEntityType: User, id }));
 
 		if (!user) throw new Error('Bad Request: Unknown user id provided.');
 
 		if (operation === PasswordOperation.REGISTER) {
-			// Send email verification
+			// As an example we are sending an OTP challenge to the user during registration
+			await callChildMutation('sendOTPChallenge', {}, params.info, params.ctx);
 		}
 
 		return mapUserToProfile(user);
