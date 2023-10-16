c8ee092ce2b55a493dafa181aabf009ee1f44021
Steve Keep <stephen.keep@techin.site>
2023-10-17 10:28:07 +1100

    fix tests

--- src/packages/mikroorm/src/entities/authentication.ts
+++ src/packages/mikroorm/src/entities/authentication.ts
@@ -1,5 +1,5 @@
 import { BigIntType, Entity, PrimaryKey, Property, JsonType } from '@mikro-orm/core';
-import { BaseEntity } from '../';
+import { BaseEntity } from './base-entity';
 
 import type { AuthenticationBaseEntity } from '@exogee/graphweaver-auth';
 
