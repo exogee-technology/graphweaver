6ee1442d5c240ef14a4959eea269740a32f1f638
Taylor Nodell <taylor.nodell@techin.site>
2023-11-14 15:49:04 +1100

    Adds cognito example to launcher

--- src/examples/aws-cognito/src/backend/rest-client.ts
+++
@@ -1,7 +0,0 @@
-import got from 'got';
-
-const baseUrl = process.env.REST_BASE_URL;
-
-export const fetch = async <T>(path: string) => {
-	return got.get(`${baseUrl}${path}`).json<T[]>();
-};
--- src/examples/aws-cognito/src/backend/schema/index.ts
+++ src/examples/aws-cognito/src/backend/schema/index.ts
@@ -4,7 +4,5 @@ import { cognitoUser } from '../cognito';
 const awsLambdaUtils = createAwsLambdaUtils({
 	...(process.env.AWS_LAMBDA_ENDPOINT ? { defaultEndpoint: process.env.AWS_LAMBDA_ENDPOINT } : {}),
 });
-
+console.log('awsLambdaUtils', awsLambdaUtils);
 export const resolvers = [cognitoUser ? [cognitoUser.resolver] : [], awsLambdaUtils.resolver];
-
-//if (cognitoUser) applyTeamOnlyAcl(cognitoUser.entity);
