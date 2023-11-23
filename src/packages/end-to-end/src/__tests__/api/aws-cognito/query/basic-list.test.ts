9b3f579f6ee80b35cd84fa9561836a0950c5735a
Taylor Nodell <taylor.nodell@techin.site>
2023-11-23 16:47:57 +1100

    Adds e2e tests

---
+++ src/packages/end-to-end/src/__tests__/api/aws-cognito/query/basic-list.test.ts
@@ -0,0 +1,38 @@
+import 'reflect-metadata';
+
+import request from 'supertest-graphql';
+import gql from 'graphql-tag';
+import { CognitoUser, createAwsCognitoUserResolver } from '@exogee/graphweaver-aws';
+
+import { config } from '../../../../config';
+import Graphweaver from '@exogee/graphweaver-server';
+
+if (!process.env.COGNITO_USER_POOL_ID) {
+	throw new Error('Missing COGNITO_USER_POOL_ID');
+}
+const cognitoUser = createAwsCognitoUserResolver({
+	userPoolId: process.env.COGNITO_USER_POOL_ID,
+	region: 'ap-southeast-2',
+});
+
+const graphweaver = new Graphweaver({
+	resolvers: [cognitoUser.resolver],
+});
+
+describe('basic query', () => {
+	test('should get cognito users', async () => {
+		const { data } = await request<{ cognitoUsers: CognitoUser[] }>(config.baseUrl)
+			.query(
+				gql`
+					query {
+						cognitoUsers {
+							id
+						}
+					}
+				`
+			)
+			.expectNoErrors();
+
+		expect(data?.cognitoUsers).toHaveLength(1);
+	});
+});
