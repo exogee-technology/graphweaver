bbd8f2824c3b86069e0797323a03769d09227720
Siow <siow.koon@exogee.com>
2023-12-12 14:13:58 +1100

    updated the test name

--- src/packages/end-to-end/src/__tests__/ui/issues/gh192.test.ts
+++ src/packages/end-to-end/src/__tests__/ui/issues/gh192.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { config } from '../../../config';
 
-test('ensure table refreshed after create', async ({ page }) => {
+test('ensure toast is displayed with the name of the item after creation', async ({ page }) => {
 	await page.goto(config.adminUiUrl);
 	await page.getByRole('link', { name: 'mikro-orm-sqlite' }).click();
 	await page.getByRole('link', { name: 'Album' }).click();
@@ -10,12 +10,14 @@ test('ensure table refreshed after create', async ({ page }) => {
 	await expect(page.getByText('A Cor Do Som')).toBeVisible();
 	await page.getByText('A Cor Do Som').click();
 
-	// pre-condition: Infinity row does not exist in the table
-	await expect(page.getByText('Infinity')).not.toBeVisible();
+	const title = 'Infinity';
+
+	// pre-condition: Infinity text not found on the page
+	await expect(page.getByText(title)).not.toBeVisible();
 
 	// create new album called Infinity
 	await page.getByRole('button', { name: 'Create New Album' }).click();
-	await page.getByRole('textbox', { name: 'title' }).fill('Infinity');
+	await page.getByRole('textbox', { name: 'title' }).fill(title);
 
 	await expect(page.locator('form').locator('_react=Select', { hasText: 'artist' })).toBeVisible();
 	await page.locator('form').locator('_react=Select', { hasText: 'artist' }).click();
@@ -24,6 +26,6 @@ test('ensure table refreshed after create', async ({ page }) => {
 	await page.locator('form').getByText('#9 Dream').click();
 	await page.getByRole('button', { name: 'Save' }).click();
 
-	// post-condition: Infinity row does exist in the table
-	await expect(page.getByText('Infinity')).toBeVisible();
+	// post-condition: Infinity text found on the page
+	await expect(page.getByText(title)).toBeVisible();
 });
