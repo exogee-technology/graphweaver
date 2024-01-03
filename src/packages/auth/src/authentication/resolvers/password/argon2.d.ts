3ee33307a823179dcf0dc750298c74a2429906ac
Dylan Thorne <dylan.thorne@gmail.com>
2024-01-03 13:14:10 +1100

    use argon2-browser

---
+++ src/packages/auth/src/authentication/resolvers/password/argon2.d.ts
@@ -0,0 +1,1 @@
+declare module 'argon2-browser';
--- src/packages/auth/src/authentication/resolvers/password/resolver.ts
+++ src/packages/auth/src/authentication/resolvers/password/resolver.ts
@@ -1,5 +1,5 @@
 import { BackendProvider, Resolver } from '@exogee/graphweaver';
-import * as argon2 from 'argon2';
+import * as argon2 from 'argon2-browser';
 
 import { PasswordStorage } from '../../entities';
 import { createBasePasswordAuthResolver } from './base-resolver';
