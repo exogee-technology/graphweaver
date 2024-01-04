786b708fa9240f3ed62c6d31c89c1af304f373ae
Steve Keep <stephen.keep@techin.site>
2024-01-04 12:13:25 +1100

    remove console logs

--- src/packages/builder/src/build/types.ts
+++ src/packages/builder/src/build/types.ts
@@ -5,26 +5,22 @@ import path from 'path';
 import { codeGenerator } from '../codegen';
 
 const importResolvers = async () => {
-	const resolverPath = path.join(process.cwd(), './dist/backend/index.js');
+	const resolverPath = path.join(process.cwd(), './dist/backend/index');
 	const { resolvers } = await import(resolverPath);
 	return resolvers;
 };
 
 export const buildTypes = async () => {
-	console.log('building types');
 	const resolvers = await importResolvers();
 	const schema = await buildSchema({
 		resolvers,
 	});
-	console.log(schema);
 	const files = await codeGenerator(schema);
 	const types = files?.find((file) => file.filename === 'src/types.generated.ts')?.content;
 
 	if (!types) {
 		throw new Error('Could not generate types');
 	}
-
-	console.log(types);
 
 	const typesPath = './.graphweaver/types.ts';
 	fs.promises.writeFile(typesPath, types, 'utf8');
