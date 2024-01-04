c9d040c6fc055cdc877b035b61542142eb472755
Steve Keep <stephen.keep@techin.site>
2024-01-04 13:07:12 +1100

    add types bin

---
+++ src/packages/cli/bin/gw-types.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env tsx
+
+import 'reflect-metadata';
+import { buildSchema } from '@exogee/graphweaver';
+import { codeGenerator } from '@exogee/graphweaver-builder';
+import fs from 'fs';
+import path from 'path';
+
+const importResolvers = async () => {
+	const resolverPath = path.join(process.cwd(), './src/backend/schema/index');
+	const { resolvers } = await import(resolverPath);
+
+	if (!resolvers) {
+		throw new Error(
+			'Could not generate types please export your resolvers from the schema index file'
+		);
+	}
+
+	return resolvers;
+};
+
+export const buildTypes = async () => {
+	const resolvers = await importResolvers();
+	const schema = await buildSchema({
+		resolvers,
+	});
+	const files = await codeGenerator(schema);
+	const types = files?.find((file) => file.filename === 'src/types.generated.ts')?.content;
+
+	if (!types) {
+		throw new Error('Could not generate types');
+	}
+
+	const typesPath = './.graphweaver/types.ts';
+	fs.promises.writeFile(typesPath, types, 'utf8');
+};
+
+buildTypes();
