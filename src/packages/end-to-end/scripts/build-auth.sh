bc1fd3a64873d4e6fced0146728c52032d9372b2
Dylan Thorne <dylan.thorne@gmail.com>
2024-01-04 13:08:32 +1100

    use rest backend to init

--- src/packages/end-to-end/scripts/build-auth.sh
+++ src/packages/end-to-end/scripts/build-auth.sh
@@ -1,5 +1,5 @@
 rm -rf ./app 
-node ../cli/bin init --name=app --useVersion=\"local\" 
+node ../cli/bin init --name=app --useVersion=\"local\" --backend=rest
 cd app 
 pnpm link ../../auth
 pnpm i --ignore-workspace --no-lockfile
