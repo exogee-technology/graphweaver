630da981d98891b4501b857586e061b9fbe1b7ad
Taylor Nodell <taylor.nodell@techin.site>
2024-01-05 16:32:38 +1100

    Gets example project running generating signed urls

--- src/examples/s3-storage/src/backend/schema/index.ts
+++ src/examples/s3-storage/src/backend/schema/index.ts
@@ -1,3 +1,4 @@
 import { UserResolver } from './user';
+import { UploadResolver } from '@exogee/graphweaver-storage-provider';
 
-export const resolvers = [UserResolver];
+export const resolvers = [UserResolver, UploadResolver];
