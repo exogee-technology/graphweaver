73e92b0a318b26a35159c733853bac9626ee94ee
Taylor Nodell <taylor.nodell@techin.site>
2024-01-08 14:42:39 +1100

    Implements diagram, working

--- src/examples/s3-storage/src/admin-ui/custom-pages/index.tsx
+++ src/examples/s3-storage/src/admin-ui/custom-pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { gql, useMutation } from '@apollo/client';
 
 export const customPages = {
@@ -16,19 +16,73 @@ const generateUploadUrlMutation = gql`
 	}
 `;
 
+const createSubmissionMutation = gql`
+	mutation Mutation($createSubmissionData: SubmissionInsertInput!) {
+		createSubmission(data: $createSubmissionData) {
+			id
+			url
+		}
+	}
+`;
+
 const GetSignedURLComponent = () => {
-	console.log('Hey there');
-	const [func, { data }] = useMutation(generateUploadUrlMutation);
+	const [getPresignedURL, { data }] = useMutation(generateUploadUrlMutation);
+	const [createSubmission] = useMutation(createSubmissionMutation);
 
-	useEffect(() => {
-		// Call the mutation with the "testKey" variable
-		func({ variables: { key: 'testKey' } });
-	}, [func]);
+	const handleFileUpload = async (file: any) => {
+		const res = await getPresignedURL({ variables: { key: file.name } });
+		console.log(res);
 
-	// Access the mutation result through the `data` object
-	console.log(data);
+		const uploadURL = res.data.generateUploadUrl;
 
-	return <div>nice component</div>;
+		if (!uploadURL) {
+			console.error('Upload URL is not available');
+			return;
+		}
+
+		try {
+			const response = await fetch(uploadURL, {
+				method: 'PUT',
+				body: file,
+				headers: {
+					// This file.type isn't getting uploaded to S3
+					'Content-Type': file.type,
+				},
+			});
+			console.log(response);
+			if (response.ok) {
+				console.log('File uploaded successfully!');
+				// Now we can save the url to a new submission entity
+				const submission = await createSubmission({
+					variables: {
+						createSubmissionData: {
+							url: uploadURL,
+						},
+					},
+				});
+
+				console.log(submission);
+			} else {
+				console.error('Error uploading file:', response.statusText);
+			}
+		} catch (error) {
+			console.error('Error:', error);
+		}
+	};
+
+	const handleFileInputChange = (event: React.ChangeEventHandler<HTMLInputElement>) => {
+		const file = event.target.files[0];
+		if (file) {
+			handleFileUpload(file);
+		}
+	};
+
+	return (
+		<div>
+			<div>nice component</div>
+			<input type="file" onChange={handleFileInputChange} />
+		</div>
+	);
 };
 
 export default GetSignedURLComponent;
--- src/packages/graphweaver-storage-provider/src/generateUploadUrl.ts
+++ src/packages/graphweaver-storage-provider/src/generateUploadUrl.ts
@@ -1,23 +1,16 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { Arg, createBaseResolver, ID, Mutation, Resolver } from '@exogee/graphweaver';
+import { Arg, ID, Mutation, Resolver } from '@exogee/graphweaver';
 
-// This entity will have to be defined in the project that uses this package.
-// import { SubmissionDataEntity } from './data-entity';
-// import { Submission } from './entity';
-// import { SubmissionProvider } from './provider';
-
-// @Resolver(() => Submission)
+//storage resolver
 export class UploadResolver {
+	//Shunk this out
 	@Mutation(() => String)
 	async generateUploadUrl(@Arg('key', () => ID) key: string): Promise<string> {
 		if (!process.env.AWS_S3_BUCKET) throw new Error('Missing required env AWS_S3_BUCKET');
 
-		const endpoint = process.env.AWS_S3_ENDPOINT;
-
 		const s3 = new S3Client({
-			region: process.env.AWS_DEFAULT_REGION ?? 'ap-southeast-2',
-			...(endpoint ? { endpoint } : {}),
+			region: process.env.AWS_DEFAULT_REGION,
 		});
 
 		const command = new PutObjectCommand({
