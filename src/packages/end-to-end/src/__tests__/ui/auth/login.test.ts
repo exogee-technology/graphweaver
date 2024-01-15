03d84913d9c3c8b01575627fcc0cb10db43e77dc
Steve Keep <stephen.keep@techin.site>
2024-01-16 09:11:03 +1100

    add auth ui test runner

--- src/packages/end-to-end/src/__tests__/ui/auth/login.test.ts
+++ src/packages/end-to-end/src/__tests__/ui/auth/login.test.ts
@@ -1,10 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { config } from '../../../config';
 
-test('Expand datasource dropdown', async ({ page }) => {
+test('should allow a successful login', async ({ page }) => {
 	await page.goto(config.adminUiUrl);
-	await page.getByRole('link', { name: 'mikro-orm-sqlite' }).click();
-	await page.getByRole('link', { name: 'Album' }).click();
 
-	await expect(page.getByRole('heading', { name: 'Album' })).toBeVisible();
+	await page.getByPlaceholder('Username').click();
+	await page.getByPlaceholder('Username').fill('darth');
+	await page.getByPlaceholder('Password').click();
+	await page.getByPlaceholder('Password').fill('deathstar123');
+	await page.getByRole('button', { name: 'Login' }).click();
+	await expect(page.getByRole('link', { name: 'mikro-orm-my' })).toBeVisible();
 });
