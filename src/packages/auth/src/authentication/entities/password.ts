14a94c4342cac8c163d4de72fd62daeab64122b8
Steve Keep <stephen.keep@techin.site>
2024-01-15 16:16:52 +1100

    add acl to credential entity

--- src/packages/auth/src/authentication/entities/password.ts
+++ src/packages/auth/src/authentication/entities/password.ts
@@ -2,11 +2,14 @@ import {
 	BaseDataEntity,
 	Field,
 	GraphQLEntity,
+	GraphqlEntityType,
 	ID,
 	ObjectType,
 	ReadOnly,
 	SummaryField,
 } from '@exogee/graphweaver';
+import { AccessControlList, AuthorizationContext } from '../../types';
+import { ApplyAccessControlList } from '../../decorators';
 
 export interface PasswordStorage {
 	id: string;
@@ -14,15 +17,22 @@ export interface PasswordStorage {
 	password?: string;
 }
 
-@ReadOnly()
-@ObjectType('Credential')
-export class Credential<D extends BaseDataEntity> extends GraphQLEntity<D> {
-	public dataEntity!: D;
+export const createCredentialEntity = <D extends BaseDataEntity>(
+	acl?: AccessControlList<Credential, AuthorizationContext>
+) => {
+	@ApplyAccessControlList(acl ?? {})
+	@ReadOnly()
+	@ObjectType('Credential')
+	class Credential extends GraphQLEntity<D> {
+		public dataEntity!: D;
 
-	@Field(() => ID)
-	id!: string;
+		@Field(() => ID)
+		id!: string;
 
-	@SummaryField()
-	@Field(() => String)
-	username!: string;
-}
+		@SummaryField()
+		@Field(() => String)
+		username!: string;
+	}
+
+	return Credential as GraphqlEntityType<Credential, D>;
+};
