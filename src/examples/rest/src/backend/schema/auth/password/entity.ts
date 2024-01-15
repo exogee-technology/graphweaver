90ce6bc0696ed23e00b0b2b567a792b704f1ee7c
Steve Keep <stephen.keep@techin.site>
2024-01-15 16:26:45 +1100

    update the acl

--- src/examples/rest/src/backend/schema/auth/password/entity.ts
+++ src/examples/rest/src/backend/schema/auth/password/entity.ts
@@ -8,7 +8,7 @@ import { Credential as OrmCredential } from '../../../entities';
 const acl: AccessControlList<Credential, AuthorizationContext> = {
 	LIGHT_SIDE: {
 		// Users can only perform operations on their own tasks
-		all: (context) => ({ id: context.user?.id }),
+		read: (context) => ({ id: context.user?.id }),
 	},
 	DARK_SIDE: {
 		// Dark side user role can perform operations on any tasks
