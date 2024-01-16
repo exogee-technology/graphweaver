4f5cce56a9e3cdf3c6e3d453110d47f5f25c0002
Taylor Nodell <taylor.nodell@techin.site>
2024-01-16 11:42:55 +1100

    Working provider at top of Submisson

--- src/examples/s3-storage/src/backend/schema/submission/entity.ts
+++ src/examples/s3-storage/src/backend/schema/submission/entity.ts
@@ -2,7 +2,13 @@ import { GraphQLEntity, Field, ID, ObjectType, Root } from '@exogee/graphweaver'
 import { S3StorageProvider, StorageType } from '@exogee/graphweaver-storage-provider';
 
 import { Submission as OrmSubmission } from '../../entities';
-import { GraphQLJSON } from '@exogee/graphweaver-scalars';
+
+const s3 = new S3StorageProvider({
+	bucketName: process.env.AWS_S3_BUCKET,
+	region: process.env.AWS_REGION,
+	type: StorageType.S3,
+	expiresIn: 3600,
+});
 
 @ObjectType('Submission')
 export class Submission extends GraphQLEntity<OrmSubmission> {
@@ -14,27 +20,8 @@ export class Submission extends GraphQLEntity<OrmSubmission> {
 	@Field(() => String)
 	key!: string;
 
-	@Field(() => GraphQLJSON)
-	meta(@Root() Submission: Submission) {
-		return {
-			awsBucket: 'graphweaver-test',
-			awsRegion: 'ap-southeast-2',
-			awsKey: Submission.key,
-		};
-	}
-
 	@Field(() => String, { nullable: true })
 	downloadUrl(@Root() submission: Submission) {
-		if (!submission.meta(submission).awsBucket) throw new Error('Missing required AWS Bucket');
-		if (!submission.meta(submission).awsRegion) throw new Error('Missing required AWS Region');
-
-		const s3 = new S3StorageProvider({
-			bucketName: submission.meta(submission).awsBucket,
-			region: submission.meta(submission).awsRegion,
-			type: StorageType.S3,
-			expiresIn: 3600,
-		});
-
 		return s3.getDownloadUrl(submission.key);
 	}
 }
