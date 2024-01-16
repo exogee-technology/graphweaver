2511051967b0fcff028aed676f897f53d2f6e49f
Taylor Nodell <taylor.nodell@techin.site>
2024-01-16 15:10:39 +1100

    Removes type files from src

--- src/packages/graphweaver-storage-provider/src/decorators/download-url.d.ts
+++
@@ -1,7 +0,0 @@
-import { IStorageProvider } from '../storageProvider';
-type DownloadUrlFieldOptions = {
-    storageProvider: IStorageProvider;
-    key: string;
-};
-export declare function DownloadUrlField({ storageProvider, key, }: DownloadUrlFieldOptions): PropertyDecorator;
-export {};
