9ef578d3546475aae51a7a3052718ed36b272f20
Taylor Nodell <taylor.nodell@techin.site>
2024-01-16 16:51:41 +1100

    Starts getting media type into admin ui

--- src/packages/graphweaver-storage-provider/src/decorators/download-url.ts
+++ src/packages/graphweaver-storage-provider/src/decorators/download-url.ts
@@ -1,18 +1,62 @@
+import { getMetadataStorage } from '@exogee/graphweaver';
 import { IStorageProvider } from '../storageProvider';
+import { findType } from 'type-graphql/dist/helpers/findType';
+
+export enum MediaType {
+	IMAGE = 'Image',
+	VIDEO = 'Video',
+}
 
 type DownloadUrlFieldOptions = {
 	storageProvider: IStorageProvider;
 	key: string;
+	mediaType: MediaType;
 };
 
 export function DownloadUrlField({
 	storageProvider,
 	key,
+	mediaType,
 }: DownloadUrlFieldOptions): PropertyDecorator {
 	return function (target: any, propertyKey: string | symbol) {
+		// if propertyKey is a symbol, throw an error
+		if (typeof propertyKey === 'symbol') {
+			throw new Error(`@DownloadUrlField decorator key must be a string.`);
+		}
+		console.log('target', target);
+
+		const { getType, typeOptions } = findType({
+			metadataKey: 'design:returntype',
+			prototype: target,
+			propertyKey,
+			returnTypeFunc: () => String,
+			typeOptions: { nullable: true },
+		});
+
+		const prototype = target.constructor.prototype;
+
+		const options = {
+			name: mediaType,
+			description: undefined,
+			complexity: undefined,
+			deprecationReason: undefined,
+			simple: undefined,
+		};
+
+		getMetadataStorage().collectClassFieldMetadata({
+			name: propertyKey,
+			schemaName: options.name || propertyKey,
+			getType,
+			typeOptions,
+			complexity: options.complexity,
+			target: prototype.constructor,
+			description: options.description,
+			deprecationReason: options.deprecationReason,
+			simple: options.simple,
+		});
 		Object.defineProperty(target, propertyKey, {
 			get: function () {
-				// Check that key is a key on the object
+				// Check that key is a property on the object
 				if (!target[key]) {
 					throw new Error(`@DownloadUrlField decorator key must be a key on the object.`);
 				}
