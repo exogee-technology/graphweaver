e2fbe3913e39dc6aed3ca80007e1a7c532c694f9
Adam Gawronski <adam.gawronski@exogee.com>
2024-01-23 17:21:03 +1100

    Complete TS script for checking for native dependencies

--- src/packages/end-to-end/scripts/check-for-native-deps.ts
+++ src/packages/end-to-end/scripts/check-for-native-deps.ts
@@ -1,5 +1,6 @@
 import { readdirSync, statSync, existsSync } from 'fs';
 import { join, extname } from 'path';
+import { execSync } from 'child_process';
 
 const findFiles = (dir: string, ext: string, fileList: string[] = []) => {
 	const files = readdirSync(dir);
@@ -18,19 +19,49 @@ const findFiles = (dir: string, ext: string, fileList: string[] = []) => {
 	return fileList;
 };
 
+const extractPackageName = (path: string) => {
+	const splitPath = path.split('/');
+	const lastNodeModulesIndex = splitPath.lastIndexOf('node_modules');
+	const packageName = splitPath[lastNodeModulesIndex + 1];
+	console.log('Extracted package name:', path, packageName);
+	return packageName;
+};
+
+const runPnpmWhy = (packageName: string) => {
+	const command = `pnpm why -r -P ${packageName}`;
+	const output = execSync(command, { cwd: './app' }).toString();
+	return output;
+};
+
+// This is the root node_modules directory for the built standalone app
 const dir = './app/node_modules';
 
 if (!existsSync(dir)) {
 	console.log(
 		`${dir} directory does not exist. Please run pnpm test-build or one of the pnpm dev-* commands first (to create a test app) then run this command again`
 	);
+	process.exit(0);
 }
 
 const nativeModules = findFiles(dir, '.node');
-if (nativeModules.length > 0) {
+let output = '';
+
+nativeModules.forEach((filePath) => {
+	const packageName = extractPackageName(filePath);
+	const whyResult = runPnpmWhy(packageName);
+	if (whyResult) {
+		output += `Why ${packageName} exists in the project:`;
+		output += whyResult + '\n\n';
+	}
+});
+
+if (output) {
 	console.log(
-		'Found native modules files! Please run a "pnpn why" on the package names to find the relevant dependency'
+		`Found native modules in production dependencies! See the "pnpm why" output below for details:`
 	);
-	console.log(nativeModules.join('\n'));
+	console.log(output);
 	process.exit(1);
+} else {
+	// No native modules found, hurrah!
+	process.exit(0);
 }
