64984262e9ffd8382dd0b3c2fb15f6c89abfb1f8
Dylan Thorne <dylan.thorne@gmail.com>
2024-01-29 16:40:37 +1100

    change from execsync to await exec

--- src/packages/cli/src/types.ts
+++ src/packages/cli/src/types.ts
@@ -1,5 +1,29 @@
-import { execSync } from 'child_process';
+import { exec } from 'child_process';
+
+const asyncExec = async (command: string) => {
+	const output = await new Promise((resolve, reject) => {
+		const childProcess = exec(command);
+		if (childProcess.stdout)
+			childProcess.stdout.on('data', (data) => {
+				resolve(`exec success: ${data}`);
+			});
+		if (childProcess.stderr)
+			childProcess.stderr.on('data', (data) => {
+				resolve(`stderr error: ${data}`);
+			});
+		childProcess.on('close', (code) => {
+			resolve(`exec success: 'close'  ${code}`);
+		});
+		childProcess.on('exit', (code) => {
+			resolve(`exec success: 'exit' ${code}`);
+		});
+		childProcess.on('error', (error) => {
+			resolve(`exec error: ${error}`);
+		});
+	});
+	return output;
+};
 
 export const generateTypes = async () => {
-	execSync(`gw-types`, { stdio: 'inherit' });
+	await asyncExec(`gw-types`);
 };
