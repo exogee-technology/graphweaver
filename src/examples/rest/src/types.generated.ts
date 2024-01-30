cc8b5772072778dde9c4e66372c6595499c90803
Steve Keep <stephen.keep@techin.site>
2024-01-30 15:46:10 +1100

    fix auth check test

--- src/examples/rest/src/types.generated.ts
+++ src/examples/rest/src/types.generated.ts
@@ -92,6 +92,9 @@ export type AdminUiMetadata = {
 export type ApiKey = {
   __typename?: 'ApiKey';
   id: Scalars['ID']['output'];
+  key: Scalars['String']['output'];
+  revoked: Scalars['Boolean']['output'];
+  roles?: Maybe<Array<Scalars['String']['output']>>;
 };
 
 export type ApiKeyCreateOrUpdateInput = {
@@ -119,10 +122,29 @@ export type ApiKeysListFilter = {
   id_nin?: InputMaybe<Array<Scalars['ID']['input']>>;
   id_notnull?: InputMaybe<Scalars['ID']['input']>;
   id_null?: InputMaybe<Scalars['ID']['input']>;
+  key?: InputMaybe<Scalars['String']['input']>;
+  key_ilike?: InputMaybe<Scalars['String']['input']>;
+  key_in?: InputMaybe<Array<Scalars['String']['input']>>;
+  key_like?: InputMaybe<Scalars['String']['input']>;
+  key_ne?: InputMaybe<Scalars['String']['input']>;
+  key_nin?: InputMaybe<Array<Scalars['String']['input']>>;
+  key_notnull?: InputMaybe<Scalars['String']['input']>;
+  key_null?: InputMaybe<Scalars['String']['input']>;
+  revoked?: InputMaybe<Scalars['Boolean']['input']>;
+  roles?: InputMaybe<Scalars['String']['input']>;
+  roles_ilike?: InputMaybe<Scalars['String']['input']>;
+  roles_in?: InputMaybe<Array<Scalars['String']['input']>>;
+  roles_like?: InputMaybe<Scalars['String']['input']>;
+  roles_ne?: InputMaybe<Scalars['String']['input']>;
+  roles_nin?: InputMaybe<Array<Scalars['String']['input']>>;
+  roles_notnull?: InputMaybe<Scalars['String']['input']>;
+  roles_null?: InputMaybe<Scalars['String']['input']>;
 };
 
 export type ApiKeysOrderByInput = {
   id?: InputMaybe<Sort>;
+  key?: InputMaybe<Sort>;
+  roles?: InputMaybe<Sort>;
 };
 
 export type ApiKeysPaginationInput = {
--- src/packages/end-to-end/src/__tests__/api/auth/api-key/api-key.test.ts
+++ src/packages/end-to-end/src/__tests__/api/auth/api-key/api-key.test.ts
@@ -12,13 +12,7 @@ import {
 	Resolver,
 	createBaseResolver,
 } from '@exogee/graphweaver';
-import {
-	authApolloPlugin,
-	UserProfile,
-	createApiKeyResolver,
-	ApiKey,
-	ApiKeyStorage,
-} from '@exogee/graphweaver-auth';
+import { authApolloPlugin, UserProfile, ApiKeyStorage } from '@exogee/graphweaver-auth';
 import { MikroBackendProvider, BaseEntity, ConnectionManager } from '@exogee/graphweaver-mikroorm';
 import { SqliteDriver } from '@mikro-orm/sqlite';
 import { ArrayType, BigIntType, Entity, PrimaryKey, Property } from '@mikro-orm/core';
@@ -51,20 +45,6 @@ export class OrmTask extends BaseEntity {
 	description!: string;
 }
 
-// Create Data Provider
-const connection = {
-	connectionManagerId: 'InMemory',
-	mikroOrmConfig: {
-		entities: [OrmApiKey, OrmTask],
-		dbName: ':memory:',
-		driver: SqliteDriver,
-	},
-};
-const dataProvider = new MikroBackendProvider(OrmApiKey, connection);
-
-@Resolver()
-export class ApiKeyAuthResolver extends createApiKeyResolver<OrmApiKey>(ApiKey, dataProvider) {}
-
 @ObjectType('Task')
 export class Task extends GraphQLEntity<OrmTask> {
 	public dataEntity!: OrmTask;
@@ -76,14 +56,26 @@ export class Task extends GraphQLEntity<OrmTask> {
 	description!: string;
 }
 
+// Create Data Provider
+const connection = {
+	connectionManagerId: 'InMemory',
+	mikroOrmConfig: {
+		entities: [OrmApiKey, OrmTask],
+		dbName: ':memory:',
+		driver: SqliteDriver,
+	},
+};
+
 @Resolver((of) => Task)
 class TaskResolver extends createBaseResolver<Task, OrmTask>(
 	Task,
 	new MikroBackendProvider(OrmTask, connection)
 ) {}
 
+const dataProvider = new MikroBackendProvider(OrmApiKey, connection);
+
 const graphweaver = new Graphweaver({
-	resolvers: [ApiKeyAuthResolver, TaskResolver],
+	resolvers: [TaskResolver],
 	apolloServerOptions: {
 		plugins: [authApolloPlugin(async () => ({} as UserProfile), dataProvider)],
 	},
@@ -94,6 +86,8 @@ describe('API Key Authentication', () => {
 		await ConnectionManager.connect('InMemory', connection);
 		const database = ConnectionManager.database('InMemory');
 		await database?.orm.schema.createSchema();
+
+		// Standard working test API Key
 		const testApiKey = database?.em.create(OrmApiKey, {
 			key: 'test',
 			secret:
@@ -103,6 +97,7 @@ describe('API Key Authentication', () => {
 		});
 		if (testApiKey) database?.em.persistAndFlush(testApiKey);
 
+		// Revoked test API Key
 		const testRevokedApiKey = database?.em.create(OrmApiKey, {
 			key: 'test_revoked',
 			secret:
@@ -115,7 +110,6 @@ describe('API Key Authentication', () => {
 
 	test('should return a E0001 error when no system user is found.', async () => {
 		const base64EncodedCredentials = Buffer.from('test_fail:test').toString('base64');
-		console.log(base64EncodedCredentials);
 
 		const response = await graphweaver.server.executeOperation({
 			http: { headers: new Headers({ ['x-api-key']: base64EncodedCredentials }) } as any,
@@ -137,7 +131,6 @@ describe('API Key Authentication', () => {
 
 	test('should return a E0002 error when API Key has been revoked.', async () => {
 		const base64EncodedCredentials = Buffer.from('test_revoked:test').toString('base64');
-		console.log(base64EncodedCredentials);
 
 		const response = await graphweaver.server.executeOperation({
 			http: { headers: new Headers({ ['x-api-key']: base64EncodedCredentials }) } as any,
@@ -159,7 +152,6 @@ describe('API Key Authentication', () => {
 
 	test('should return a E0003 error when password does not match.', async () => {
 		const base64EncodedCredentials = Buffer.from('test:test_fail').toString('base64');
-		console.log(base64EncodedCredentials);
 
 		const response = await graphweaver.server.executeOperation({
 			http: { headers: new Headers({ ['x-api-key']: base64EncodedCredentials }) } as any,
@@ -181,7 +173,6 @@ describe('API Key Authentication', () => {
 
 	test('should return data when using a valid system user.', async () => {
 		const base64EncodedCredentials = Buffer.from('test:test').toString('base64');
-		console.log(base64EncodedCredentials);
 
 		const response = await graphweaver.server.executeOperation({
 			http: { headers: new Headers({ ['x-api-key']: base64EncodedCredentials }) } as any,
