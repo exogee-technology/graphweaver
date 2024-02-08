316de73125215d76082f3795e1c3b0878263d035
Adam Gawronski <adam.gawronski@exogee.com>
2024-02-08 13:34:09 +1100

    Try convert builder package to ESM module

--- src/packages/builder/build.js
+++ src/packages/builder/build.js
@@ -1,12 +1,27 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { exec: nodeExec } = require('child_process');
-const { promisify } = require('util');
+// import { exec as nodeExec } from 'child_process';
 
-const { build } = require('esbuild');
-const { dependencies, devDependencies } = require('./package.json');
-const path = require('path');
+// const { exec: nodeExec } = require('child_process');
+import { promisify } from 'util';
+// const { promisify } = require('util');
 
-const exec = promisify(nodeExec);
+import { readFileSync } from 'fs';
+import { build } from 'esbuild';
+// const { build } = require('esbuild');
+
+// eslint-disable-next-line prettier/prettier
+// import { dependencies, devDependencies } from "./package.json" with { type: "json" };
+// const { dependencies, devDependencies } = require('./package.json');
+// const path = require('path');
+
+// import { path } from 'path';
+
+// const exec = promisify(nodeExec);
+
+// Load the contents of package.json into a variable
+const loadJSON = (path) => JSON.parse(readFileSync(new URL(path, import.meta.url)));
+
+const pjson = loadJSON('./package.json');
 
 (async () => {
 	await build({
@@ -19,8 +34,8 @@ const exec = promisify(nodeExec);
 		external: [
 			// Our dependencies will be installed in node_modules. The bundling is
 			// just to ensure our own files are pulled in
-			...Object.keys(dependencies),
-			...Object.keys(devDependencies),
+			...Object.keys(pjson.dependencies),
+			...Object.keys(pjson.devDependencies),
 		],
 	});
 })();
