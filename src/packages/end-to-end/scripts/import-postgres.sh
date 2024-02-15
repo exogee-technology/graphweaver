f46a5500e0108cf9777b0d03550058475dafa600
Steve Keep <stephen.keep@techin.site>
2024-02-15 16:15:27 +1100

    merge build test with postgres

--- src/packages/end-to-end/scripts/import-postgres.sh
+++ src/packages/end-to-end/scripts/import-postgres.sh
@@ -1,5 +1,7 @@
 rm -rf ./app 
 node ../cli/bin init --name=app --backend=postgres --useVersion=\"local\" 
 cd app 
+pnpm link ../../auth
 pnpm i --ignore-workspace --no-lockfile
 node ../../cli/bin import postgresql --database=gw --user=postgres --password=postgres --host=localhost --port=5432 --o
+pnpm build
