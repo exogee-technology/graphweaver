99435145cab8d553c30b1519a32efdbee3fb55b1
Steve Keep <stephen.keep@techin.site>
2024-02-15 16:43:54 +1100

    convert admin-ui to not bundle

--- src/packages/admin-ui/build.js
+++ src/packages/admin-ui/build.js
@@ -7,28 +7,15 @@ const flags = process.argv.slice(0);
 const flagIncludes = (flagName) => !!flags.find((flag) => flag === `--${flagName}`);
 
 (async () => {
+	const { glob } = await import('glob');
+	const entryPoints = await glob('./src/**/*.{ts,tsx,css}');
+
 	await esbuild.build({
 		outdir: 'dist',
 		format: 'esm',
-		bundle: true,
 		minify: false,
 		sourcemap: 'linked',
-		external: [
-			// This can't be bundled because it's virtual and supplied by
-			// our vite plugin directly.
-			'virtual:graphweaver-user-supplied-custom-pages',
-
-			// And these can't because they're peer dependencies, and we need to use
-			// the version supplied by the ultimate consumer of the library.
-			'graphql',
-			'react',
-			'react-dom',
-			'react-router',
-			'react-router-dom',
-			'@remix-run/router',
-			'@exogee/graphweaver-admin-ui-components',
-		],
-		entryPoints: ['src/main.tsx'],
+		entryPoints,
 		plugins: [
 			cssModulesPlugin({ inject: true }),
 			svgrPlugin({ exportType: 'named' }),
