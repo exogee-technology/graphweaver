a163414f5a255286bd20242f9e7aaa79d3cfa8f2
Steve Keep <stephen.keep@techin.site>
2024-02-15 11:47:29 +1100

    update script with local references

--- src/packages/end-to-end/scripts/import-sqlite.ts
+++ src/packages/end-to-end/scripts/import-sqlite.ts
@@ -13,24 +13,44 @@ async function copyFile(source: string, destination: string) {
 	await fs.promises.copyFile(source, destination);
 }
 
-async function copyDirectory(source: string, destination: string) {
-	await fs.promises.cp(source, destination, { recursive: true, force: true });
-}
-
 async function main() {
 	try {
 		process.chdir('../../');
 
+		// Publish all local packages
 		execSync('pnpm publish:local', { stdio: 'inherit' });
-		await fs.promises.mkdir(path.join('./packs/graphweaver/node_modules'), { recursive: true });
-		await copyDirectory('./packs/@exogee', './packs/graphweaver/node_modules/@exogee');
-		await copyDirectory(
-			'./packs/vite-plugin-graphweaver',
-			'./packs/graphweaver/node_modules/vite-plugin-graphweaver'
-		);
+
+		// get list of all directories in the packs directory
+		const packDirectories = await fs.promises.readdir('./packs/@exogee');
+
+		// Update all packs package.json references to local files
+		const packNames = [
+			'graphweaver',
+			'vite-plugin-graphweaver',
+			...packDirectories.map((dir) => `@exogee/${dir}`),
+		];
+
+		// Update the local packages with references to one another
+		for (const packName of packNames) {
+			const packageJsonPath = path.join(`packs`, packName, `package.json`);
+			const packageJson = JSON.parse(await fs.promises.readFile(packageJsonPath, 'utf-8'));
+
+			// loop through the package dependencies and update the local references
+			for (const key of Object.keys(packageJson.dependencies ?? {})) {
+				if (packName.startsWith('@exogee') && key.startsWith('@exogee')) {
+					packageJson.dependencies[key] = `file:../${key.replace('@exogee/', '')}`;
+				} else if (key.startsWith('@exogee')) {
+					packageJson.dependencies[key] = `file:../${key}`;
+				} else if (packName.startsWith('@exogee') && key === 'vite-plugin-graphweaver') {
+					packageJson.dependencies[key] = `file:../../vite-plugin-graphweaver`;
+				}
+			}
+
+			await fs.promises.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
+		}
 
 		process.chdir('./packs/graphweaver');
-		execSync('npm i', { stdio: 'inherit' });
+		execSync('pnpm i --ignore-workspace --no-lockfile', { stdio: 'inherit' });
 
 		process.chdir('../../packages/end-to-end');
 		execSync('pwd', { stdio: 'inherit' });
@@ -49,9 +69,7 @@ async function main() {
 		// Install dependencies
 		process.chdir('./app');
 		execSync('pwd', { stdio: 'inherit' });
-		await fs.promises.mkdir('node_modules');
-		await copyDirectory('../../../packs/', './node_modules');
-		execSync('npm i', { stdio: 'inherit' });
+		execSync('pnpm i --ignore-workspace --no-lockfile', { stdio: 'inherit' });
 
 		// Copy the database
 		await fs.promises.mkdir('databases');
