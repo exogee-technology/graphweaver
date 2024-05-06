21f0eb840f59795da307a73ba3d1a9d2591273ef
Steve Keep <stephenkeep+github@gmail.com>
2024-05-06 14:03:11 +1000

    fixup max depth tests

---
+++ src/packages/end-to-end/src/__tests__/api/auth/security/max-depth.test.ts
@@ -0,0 +1,154 @@
+process.env.PASSWORD_AUTH_REDIRECT_URI = '*';
+
+import 'reflect-metadata';
+import gql from 'graphql-tag';
+import assert from 'assert';
+import Graphweaver from '@exogee/graphweaver-server';
+import {
+	Field,
+	GraphQLEntity,
+	ID,
+	BaseDataProvider,
+	RelationshipField,
+	Entity,
+} from '@exogee/graphweaver';
+import { authApolloPlugin, UserProfile, ApplyAccessControlList } from '@exogee/graphweaver-auth';
+
+const user = new UserProfile({
+	id: '1',
+	roles: ['admin'],
+	displayName: 'Test User',
+});
+
+const albumDataProvider = new BaseDataProvider<any, Album>('album');
+
+@ApplyAccessControlList({
+	Everyone: {
+		all: true,
+	},
+})
+@Entity('Album', {
+	provider: albumDataProvider,
+})
+export class Album extends GraphQLEntity<any> {
+	public dataEntity!: any;
+
+	@Field(() => ID)
+	id!: number;
+
+	@Field(() => String)
+	description!: string;
+
+	@RelationshipField<Album>(() => Artist, { id: (entity) => entity.artist?.id, nullable: true })
+	artist?: Artist;
+}
+
+const artistDataProvider = new BaseDataProvider<any, Artist>('artist');
+
+@ApplyAccessControlList({
+	Everyone: {
+		all: true,
+	},
+})
+@Entity('Artist', {
+	provider: artistDataProvider,
+})
+export class Artist extends GraphQLEntity<any> {
+	public dataEntity!: any;
+
+	@Field(() => ID)
+	id!: number;
+
+	@Field(() => String)
+	description!: string;
+
+	@RelationshipField<Album>(() => [Album], { relatedField: 'artist' })
+	albums!: Album[];
+}
+
+const graphweaver = new Graphweaver({
+	apolloServerOptions: {
+		plugins: [authApolloPlugin(async () => user)],
+	},
+});
+
+describe('Security', () => {
+	test('should check the depth of a query and error when it reaches seven.', async () => {
+		const spyOnArtistDataProvider = jest.spyOn(artistDataProvider, 'find');
+		const spyOnAlbumDataProvider = jest.spyOn(albumDataProvider, 'find');
+
+		const response = await graphweaver.server.executeOperation({
+			query: gql`
+				query artists {
+					artists {
+						id
+						albums {
+							id
+							artist {
+								id
+								albums {
+									id
+									artist {
+										id
+										albums {
+											id
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			`,
+		});
+
+		expect(spyOnArtistDataProvider).not.toBeCalled();
+		expect(spyOnAlbumDataProvider).not.toBeCalled();
+
+		assert(response.body.kind === 'single');
+		expect(response.body.singleResult.errors?.[0]?.message).toContain(
+			'Query depth limit of 6 exceeded'
+		);
+	});
+
+	test('should check the depth of a query and error when it reaches seven on a fragment.', async () => {
+		const spyOnArtistDataProvider = jest.spyOn(artistDataProvider, 'find');
+		const spyOnAlbumDataProvider = jest.spyOn(albumDataProvider, 'find');
+
+		const response = await graphweaver.server.executeOperation({
+			query: gql`
+				fragment artistFragment on Artist {
+					albums {
+						id
+						artist {
+							id
+							albums {
+								id
+								artist {
+									id
+									albums {
+										id
+									}
+								}
+							}
+						}
+					}
+				}
+				query artists {
+					artists {
+						id
+						...artistFragment
+					}
+				}
+			`,
+		});
+
+		expect(spyOnArtistDataProvider).not.toBeCalled();
+		expect(spyOnAlbumDataProvider).not.toBeCalled();
+
+		assert(response.body.kind === 'single');
+		expect(response.body.singleResult.errors?.[0]?.message).toContain(
+			'Query depth limit of 6 exceeded'
+		);
+	});
+});
