0b58efe6d504fe71064f3f6e9f4f660834d095eb
Kevin Brown <kevin.brown@exogee.com>
2024-06-20 11:53:11 +1000

    Actually instead of checking and erroring, why don't we just derive the version in the template from the version in our package?

--- src/packages/mikroorm/build.js
+++ src/packages/mikroorm/build.js
@@ -1,6 +1,27 @@
+const validateMikroOrmPeerAndDevVersionsMatch = async () => {
+	const path = await import('node:path');
+	const fs = await import('node:fs/promises');
+
+	// The version of MikroORM in our dev and peer dependencies must match.
+	const { devDependencies, peerDependencies } = JSON.parse(
+		await fs.readFile(path.resolve('package.json'), 'utf-8')
+	);
+
+	// Let's make sure first that the version in dev and peer match.
+	const devVersion = devDependencies['@mikro-orm/core'];
+	const peerVersion = peerDependencies['@mikro-orm/core'];
+	if (devVersion !== peerVersion) {
+		throw new Error(
+			`The version of @mikro-orm/core in the mikroorm package's devDependencies (${devVersion}) and peerDependencies (${peerVersion}) do not match.`
+		);
+	}
+};
+
 (async () => {
 	const esbuild = await import('esbuild');
 	const { glob } = await import('glob');
+
+	await validateMikroOrmPeerAndDevVersionsMatch();
 
 	const entryPoints = await glob('./src/**/*.ts');
 	await esbuild.build({
