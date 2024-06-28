eda2fcb2e47a39b6f447c510331f3e12b5b6ecc7
Kevin Brown <kevin.brown@exogee.com>
2024-06-28 15:03:00 +1000

    Fix type on enums property in the metadata service entities.

--- src/packages/core/src/metadata-service/metadata.ts
+++ src/packages/core/src/metadata-service/metadata.ts
@@ -11,5 +11,5 @@ export class AdminUiMetadata {
 	public entities: AdminUiEntityMetadata[] = [];
 
 	@Field(() => [AdminUiEnumMetadata])
-	public enums: AdminUiEntityMetadata[] = [];
+	public enums: AdminUiEnumMetadata[] = [];
 }
