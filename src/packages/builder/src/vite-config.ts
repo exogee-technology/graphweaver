3549f98d5af5458c72e534fecc1dc4250ce94a7c
Kevin Brown <kevin.brown@exogee.com>
2024-07-01 16:34:48 +1000

    Now that we're using ESM in our component libraries, we no longer need to tell Vite to pre-bundle them. This prevents needing to clean each time we change the components.

--- src/packages/builder/src/vite-config.ts
+++ src/packages/builder/src/vite-config.ts
@@ -18,43 +18,29 @@ export const viteConfig = ({
 	rootDirectory,
 	backendUrl,
 	base = '/',
-}: ViteConfigOptions): InlineConfig => {
-	// This is a fix to the issue where the bundled components are not being optimized for esm.
-	// Issue: https://github.com/exogee-technology/graphweaver/issues/290
-	const optimizeDeps = [
-		...Object.keys(
-			requireSilent('@exogee/graphweaver-admin-ui-components/package.json').dependencies
-		),
-		...Object.keys(
-			requireSilent('@exogee/graphweaver-auth-ui-components/package.json').dependencies
-		),
-		...Object.keys(requireSilent('@exogee/graphweaver-admin-ui/package.json').dependencies),
-	];
-
-	return {
-		configFile: false,
-		root: rootDirectory,
-		base,
-		define: {
-			...(backendUrl ? { 'import.meta.env.VITE_GRAPHWEAVER_API_URL': `'${backendUrl}'` } : {}),
-			'import.meta.env.VITE_ADMIN_UI_BASE': `'${base.replace(/\/$/, '')}'`,
-		},
-		build: {
-			outDir: path.resolve(process.cwd(), '.graphweaver', 'admin-ui'),
-		},
-		server: {
-			...(host ? { host } : {}),
-			...(port ? { port } : {}),
-		},
-		optimizeDeps: {
-			include: ['react-dom/client', ...optimizeDeps],
-			exclude: [
-				// This can't be bundled because it's virtual and supplied by
-				// our vite plugin directly.
-				'virtual:graphweaver-user-supplied-custom-pages',
-				'virtual:graphweaver-user-supplied-custom-fields',
-			],
-		},
-		plugins: [react(), graphweaver()],
-	};
-};
+}: ViteConfigOptions): InlineConfig => ({
+	configFile: false,
+	root: rootDirectory,
+	base,
+	define: {
+		...(backendUrl ? { 'import.meta.env.VITE_GRAPHWEAVER_API_URL': `'${backendUrl}'` } : {}),
+		'import.meta.env.VITE_ADMIN_UI_BASE': `'${base.replace(/\/$/, '')}'`,
+	},
+	build: {
+		outDir: path.resolve(process.cwd(), '.graphweaver', 'admin-ui'),
+	},
+	server: {
+		...(host ? { host } : {}),
+		...(port ? { port } : {}),
+	},
+	optimizeDeps: {
+		include: ['react-dom/client'],
+		exclude: [
+			// This can't be bundled because it's virtual and supplied by
+			// our vite plugin directly.
+			'virtual:graphweaver-user-supplied-custom-pages',
+			'virtual:graphweaver-user-supplied-custom-fields',
+		],
+	},
+	plugins: [react(), graphweaver()],
+});
