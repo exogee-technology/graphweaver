02e67a9273fc03e0d292c5fe6ee53092e96ab285
Kevin Brown <kevin.brown@exogee.com>
2024-07-02 11:43:27 +1000

    Add additional dependencies to optimize deps config so that auth ui components will work.

--- src/packages/builder/src/vite-config.ts
+++ src/packages/builder/src/vite-config.ts
@@ -34,20 +34,27 @@ export const viteConfig = ({
 	},
 	optimizeDeps: {
 		include: [
-			'react-dom',
-			'react-dom/client',
+			// These are deps where if we don't pre-build them things stop working even though they're ESM.
+			// Not sure why, but they need to be here.
+			'formik',
+			'graphql',
 
 			// These are CJS dependencies that need to get translated to ESM before Vite will be happy with them.
 			// We used to pull all of our dependencies in automatically from package.json and force this, but
-			// optimizing deps also means they're not normal source files, so we want this to be a minimal list.
+			// optimizing deps also means they're not normal source files, so we want this to be a minimal list
+			// that decreases over time.
 			'copy-to-clipboard',
 			'graphql-deduplicator',
 			'hoist-non-react-statics',
 			'nullthrows',
 			'papaparse',
 			'prop-types',
+			'react-dom',
+			'react-dom/client',
 			'react-fast-compare',
 			'react-is',
+			'react-router-dom',
+			'react',
 			'rehackt',
 			'set-value',
 		],
