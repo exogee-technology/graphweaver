f46a163da6ccc04039e95793306189df2fb59841
Kevin Brown <kevin.brown@exogee.com>
2024-07-08 13:53:26 +1000

    Revert test subgraph name.

--- src/examples/s3-storage/src/backend/index.ts
+++ src/examples/s3-storage/src/backend/index.ts
@@ -2,5 +2,5 @@ import Graphweaver from '@exogee/graphweaver-server';
 
 import './schema';
 
-export const graphweaver = new Graphweaver({ federationSubgraphName: 'test' });
+export const graphweaver = new Graphweaver();
 export const handler = graphweaver.handler();
