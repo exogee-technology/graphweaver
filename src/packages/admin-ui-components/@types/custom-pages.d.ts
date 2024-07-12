92295c9fe3a944daf5127b9ceed5b29482d51264
Steve Keep <stephenkeep+github@gmail.com>
2024-07-12 14:21:32 +1000

    cleanup custom pages definition file

--- src/packages/admin-ui-components/@types/custom-pages.d.ts
+++ src/packages/admin-ui-components/@types/custom-pages.d.ts
@@ -1,16 +1,14 @@
 declare module 'virtual:graphweaver-user-supplied-custom-pages' {
 	import { RouteObject } from 'react-router-dom';
-	import type { LoginProps } from '@exogee/graphweaver-admin-ui-components';
-
 	export interface NavLinkExport {
 		name: string;
 		route: string;
 	}
 
 	export interface CustomPagesExport {
+		defaultRoute?: string;
 		routes: () => RouteObject[] | Promise<RouteObject[]>;
 		navLinks: () => NavLinkExport[] | Promise<NavLinkExport[]>;
-		loginProps?: LoginProps;
 		sidebarFooter: () => JSX.Element | null;
 	}
 
