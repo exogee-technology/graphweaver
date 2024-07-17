471995f922353db73a319e68a64b31e9727b9250
Kevin Brown <kevin.brown@exogee.com>
2024-07-17 15:58:02 +1000

    Hoist specific field types above relationships.

--- src/packages/admin-ui-components/src/detail-panel/component.tsx
+++ src/packages/admin-ui-components/src/detail-panel/component.tsx
@@ -51,14 +51,6 @@ const isFieldReadonly = (field: EntityField | CustomField<unknown>) =>
 const getField = ({ field, autoFocus }: { field: EntityField; autoFocus: boolean }) => {
 	const isReadonly = isFieldReadonly(field);
 
-	if (field.relationshipType) {
-		// If the field is readonly and a relationship, show a link to the entity/entities
-		if (isReadonly) {
-			return <LinkField name={field.name} field={field} />;
-		}
-		return <RelationshipField name={field.name} field={field} autoFocus={autoFocus} />;
-	}
-
 	if (field.type === 'JSON') {
 		return <JSONField name={field.name} autoFocus={autoFocus} />;
 	}
@@ -69,6 +61,14 @@ const getField = ({ field, autoFocus }: { field: EntityField; autoFocus: boolean
 
 	if (field.type === 'Media') {
 		return <MediaField field={field} autoFocus={autoFocus} />;
+	}
+
+	if (field.relationshipType) {
+		// If the field is readonly and a relationship, show a link to the entity/entities
+		if (isReadonly) {
+			return <LinkField name={field.name} field={field} />;
+		}
+		return <RelationshipField name={field.name} field={field} autoFocus={autoFocus} />;
 	}
 
 	const { enumByName } = useSchema();
