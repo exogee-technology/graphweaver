b004bdbf4e151b89289e49a76be6b3b0bef0cc5a
Steve Keep <stephenkeep+github@gmail.com>
2024-07-18 11:09:38 +1000

    first working test

--- src/packages/load-testing/src/test.ts
+++ src/packages/load-testing/src/test.ts
@@ -1,5 +1,12 @@
 import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+	vus: 10,
+	duration: '30s',
+};
 
 export default function () {
-	http.get('http://localhost:3000/');
+	http.get('http://test.k6.io');
+	sleep(1);
 }
