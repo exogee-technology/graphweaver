989e3d47495e0839b5d2106c4c3552eb62f6f1df
Steve Keep <stephenkeep+github@gmail.com>
2024-07-19 10:29:06 +1000

    add create one load test

--- src/examples/databases/src/backend/index.ts
+++ src/examples/databases/src/backend/index.ts
@@ -3,4 +3,4 @@ import Graphweaver from '@exogee/graphweaver-server';
 import './schema';
 
 export const graphweaver = new Graphweaver();
-export const handler = graphweaver.handler();
+// export const handler = graphweaver.handler();
