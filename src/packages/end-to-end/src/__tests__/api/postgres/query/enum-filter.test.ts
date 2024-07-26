412e2a07d03067b3ace14521f91bc2f42fa9c03c
Kevin Brown <kevin.brown@exogee.com>
2024-07-26 14:13:34 +1000

    Add tests for enums.

---
+++ src/packages/end-to-end/src/__tests__/api/postgres/query/enum-filter.test.ts
@@ -0,0 +1,35 @@
+import request from 'supertest-graphql';
+import gql from 'graphql-tag';
+
+import { config } from '../../../../config';
+
+type Invoice = {
+	albumId: number;
+	title: string;
+	artist: {
+		artistId: number;
+		name: string;
+	};
+};
+
+describe('enum filter', () => {
+	test('should filter Invoices by PaymentStatus', async () => {
+		const { data } = await request<{ invoices: Invoice[] }>(config.baseUrl)
+			.query(gql`
+				query Invoices($filter: InvoiceListFilter) {
+					invoices(filter: $filter) {
+						invoiceId
+						paymentStatus
+					}
+				}
+			`)
+			.variables({
+				filter: {
+					paymentStatus: 'PARTIALLY-PAID',
+				},
+			})
+			.expectNoErrors();
+
+		expect(data?.invoices).toHaveLength(2);
+	});
+});
