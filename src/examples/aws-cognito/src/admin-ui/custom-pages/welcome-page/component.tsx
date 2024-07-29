e7ef79458e32bbb640695b4ba996a2757d2c4fbf
Kevin Brown <kevin.brown@exogee.com>
2024-07-29 14:41:48 +1000

    Added welcome page for cognito example.

---
+++ src/examples/aws-cognito/src/admin-ui/custom-pages/welcome-page/component.tsx
@@ -0,0 +1,34 @@
+import {
+	Arrow,
+	Button,
+	Spacer,
+	WelcomePage as WelcomePageLayout,
+} from '@exogee/graphweaver-admin-ui-components';
+import styles from './styles.module.css';
+
+export const WelcomePage = () => (
+	<WelcomePageLayout skipPath="/CognitoUser">
+		<Spacer height={30} />
+		<h2 className={styles.heading}>AWS Cognito Example</h2>
+		<Spacer height={30} />
+		<p>This example shows how to allow user creation / editing with Graphweaver and AWS Cognito.</p>
+		<Spacer height={10} />
+		<p>
+			To get started, copy the .env.example file to just .env, add the values you get from the AWS
+			console then run pnpm start. If you're getting a 500 error from your backend, the most likely
+			cause is that you haven't set up the AWS values correctly.
+		</p>
+		<Spacer height={10} />
+		<p>
+			If you're stuck,{' '}
+			<a href="" target="_blank" rel="noopener noreferrer">
+				reach out to us on Slack
+			</a>
+			. We're here to help!
+		</p>
+		<Spacer height={30} />
+		<Button className={styles.button} href="/CognitoUser">
+			Get started! <Arrow className={styles.arrow} />
+		</Button>
+	</WelcomePageLayout>
+);
