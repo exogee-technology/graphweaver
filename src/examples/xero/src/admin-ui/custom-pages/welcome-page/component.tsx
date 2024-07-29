a0bc9173e63d4666938ad1c3278153afb32490ad
Kevin Brown <kevin.brown@exogee.com>
2024-07-29 15:45:48 +1000

    Added welcome page to Xero example.

---
+++ src/examples/xero/src/admin-ui/custom-pages/welcome-page/component.tsx
@@ -0,0 +1,23 @@
+import {
+	Arrow,
+	Button,
+	Spacer,
+	WelcomePage as WelcomePageLayout,
+} from '@exogee/graphweaver-admin-ui-components';
+import styles from './styles.module.css';
+
+export const WelcomePage = () => (
+	<WelcomePageLayout skipPath="/Account">
+		<Spacer height={30} />
+		<h2 className={styles.heading}>Xero Example</h2>
+		<Spacer height={30} />
+		<p>
+			This example shows how to authenticate with Xero and query the Xero API with Graphweaver. It
+			also adds some custom dashboards which plot the data using Nivo.
+		</p>
+		<Spacer height={30} />
+		<Button className={styles.button} href="/Account">
+			Get started! <Arrow className={styles.arrow} />
+		</Button>
+	</WelcomePageLayout>
+);
