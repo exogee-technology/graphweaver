bccb66b8b364e5a683b0780c2bd954979dc58c5c
Steve Keep <stephenkeep+github@gmail.com>
2024-08-01 15:59:43 +1000

    add end-to-end cls tests

---
+++ src/packages/end-to-end/src/__tests__/ui/auth/column-level-security.test.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+import { config } from '../../../config';
+
+test('should allow to see the priority column', async ({ page }) => {
+	await page.goto(config.adminUiUrl);
+
+	await page.getByPlaceholder('Username').click();
+	await page.getByPlaceholder('Username').fill('darth');
+	await page.getByPlaceholder('Password').click();
+	await page.getByPlaceholder('Password').fill('deathstar123');
+	await page.getByRole('button', { name: 'Login' }).click();
+
+	// Close the welcome page
+	await page.getByRole('button', { name: 'Get started!' }).click();
+
+	await page.getByRole('link', { name: 'mikro-orm-my-sql' }).click();
+	await page.getByRole('link', { name: 'Task' }).click();
+
+	await expect(await page.getByText('priority')).toBeVisible();
+});
+
+test('should not allow to see the priority column', async ({ page }) => {
+	await page.goto(config.adminUiUrl);
+
+	await page.getByPlaceholder('Username').click();
+	await page.getByPlaceholder('Username').fill('luke');
+	await page.getByPlaceholder('Password').click();
+	await page.getByPlaceholder('Password').fill('lightsaber123');
+	await page.getByRole('button', { name: 'Login' }).click();
+
+	// Close the welcome page
+	await page.getByRole('button', { name: 'Get started!' }).click();
+
+	await page.getByRole('link', { name: 'mikro-orm-my-sql' }).click();
+	await page.getByRole('link', { name: 'Task' }).click();
+
+	await expect(await page.getByText('priority')).not.toBeVisible();
+});
