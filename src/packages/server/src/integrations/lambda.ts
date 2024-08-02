0e5947c544d7849dd31caf88e24f44e7a5d76122
Kevin Brown <kevin.brown@exogee.com>
2024-08-02 13:35:48 +1000

    Makes sure we log errors thrown when calling startServerAndCreateLambdaHandler.

--- src/packages/server/src/integrations/lambda.ts
+++ src/packages/server/src/integrations/lambda.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { handlers, startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
 import { GraphweaverPlugin } from '@exogee/graphweaver';
+import { logger } from '@exogee/logger';
 import { onRequestWrapper } from './utils';
 
 export const startServerless = ({
@@ -10,17 +11,24 @@ export const startServerless = ({
 	graphweaverPlugins: Set<GraphweaverPlugin<AWSLambda.APIGatewayProxyResult>>;
 	server: ApolloServer<any>;
 }): AWSLambda.APIGatewayProxyHandler => {
-	const handler = startServerAndCreateLambdaHandler(
-		server,
-		handlers.createAPIGatewayProxyEventRequestHandler()
-	);
+	try {
+		const handler = startServerAndCreateLambdaHandler(
+			server,
+			handlers.createAPIGatewayProxyEventRequestHandler()
+		);
 
-	return (event, context) =>
-		onRequestWrapper(graphweaverPlugins, async () => {
-			const res = await handler(event, context, () => {});
-			if (!res) {
-				throw new Error('Handler Response was undefined or null.');
-			}
-			return res;
-		});
+		return (event, context) => {
+			onRequestWrapper(graphweaverPlugins, async () => {
+				const res = await handler(event, context, () => {});
+				if (!res) {
+					throw new Error('Handler Response was undefined or null.');
+				}
+				return res;
+			});
+		};
+	} catch (error) {
+		// Ensure that errors thrown in startup or are logged on the way out of here.
+		logger.error(error);
+		throw error;
+	}
 };
