a370a99fb936cdf68abba7517813ad66fe3a90d7
Kevin Brown <kevin.brown@exogee.com>
2024-08-04 12:57:43 +1000

    Add customer relationship so that filtering works.

--- src/examples/sqlite/src/backend/entities/sqlite/total-invoices-by-customer.ts
+++ src/examples/sqlite/src/backend/entities/sqlite/total-invoices-by-customer.ts
@@ -1,4 +1,5 @@
-import { BigIntType, Entity, Property } from '@mikro-orm/core';
+import { BigIntType, Entity, ManyToOne, Property, Ref } from '@mikro-orm/core';
+import { Customer } from './customer';
 
 @Entity({ expression: 'SELECT CustomerId, Total FROM TotalInvoicesByCustomer' })
 export class TotalInvoicesByCustomer {
@@ -8,4 +9,12 @@ export class TotalInvoicesByCustomer {
 	@Property({ fieldName: 'Total', type: 'NUMERIC(10,2)' })
 	// MikroORM serializes this as a string for precision, but it's a number in the database
 	total!: string;
+
+	@ManyToOne({
+		entity: () => Customer,
+		ref: true,
+		fieldName: 'CustomerId',
+		nullable: true,
+	})
+	customer?: Ref<Customer>;
 }
