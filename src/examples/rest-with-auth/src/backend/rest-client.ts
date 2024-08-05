c4068a4101156c21baacddc7ce844092e5909d10
Kevin Brown <kevin.brown@exogee.com>
2024-08-05 10:45:52 +1000

    Add better error logging on rest client.

--- src/examples/rest-with-auth/src/backend/rest-client.ts
+++ src/examples/rest-with-auth/src/backend/rest-client.ts
@@ -3,5 +3,10 @@ import got from 'got-cjs';
 const baseUrl = process.env.REST_BASE_URL;
 
 export const fetch = async <T>(path: string) => {
-	return got.get(`${baseUrl}${path}`).json<T[]>();
+	try {
+		return await got.get(`${baseUrl}${path}`).json<T[]>();
+	} catch (error) {
+		console.error(`Failed to fetch data from ${path}:`, error);
+		throw error;
+	}
 };
