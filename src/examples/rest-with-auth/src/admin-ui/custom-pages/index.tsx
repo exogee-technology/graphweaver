b18fdde9b298bc941b2d08f7d4678a6bf0dd3d76
Steve Keep <stephenkeep+github@gmail.com>
2024-08-06 15:04:35 +1000

    add authmethods to loadAuthRoutes

--- src/examples/rest-with-auth/src/admin-ui/custom-pages/index.tsx
+++ src/examples/rest-with-auth/src/admin-ui/custom-pages/index.tsx
@@ -1,4 +1,4 @@
-import { loadAuthRoutes } from '@exogee/graphweaver-auth-ui-components';
+import { loadAuthRoutes, AuthenticationMethod } from '@exogee/graphweaver-auth-ui-components';
 import { WelcomePage } from './welcome-page';
 
 export const customPages = {
@@ -8,6 +8,6 @@ export const customPages = {
 			path: '/welcome',
 			element: <WelcomePage />,
 		},
-		...loadAuthRoutes(),
+		...loadAuthRoutes([AuthenticationMethod.PASSWORD, AuthenticationMethod.FORGOTTEN_PASSWORD]),
 	],
 };
--- src/packages/auth-ui-components/src/routes.tsx
+++ src/packages/auth-ui-components/src/routes.tsx
@@ -1,37 +1,47 @@
 import { Auth, Challenge } from './pages';
 import { ForgottenPassword, PasswordLogin, ResetPassword } from './components';
 
-// enum AuthenticationMethod {
-// 	PASSWORD = 'pwd',
-// 	MAGIC_LINK = 'mgl',
-// 	ONE_TIME_PASSWORD = 'otp',
-// 	WEB3 = 'wb3',
-// 	PASSKEY = 'pky',
-// }
+export enum AuthenticationMethod {
+	PASSWORD = 'pwd',
+	API_KEY = 'api',
+	AUTH_ZERO = 'au0',
+	FORGOTTEN_PASSWORD = 'fpd',
+	MAGIC_LINK = 'mgl',
+	ONE_TIME_PASSWORD = 'otp',
+	WEB3 = 'wb3',
+	PASSKEY = 'pky',
+}
 
-export const loadAuthRoutes = () => {
+export const loadAuthRoutes = (authMethods: AuthenticationMethod[]) => {
+	const routes = new Set();
+
+	if (authMethods.includes(AuthenticationMethod.PASSWORD)) {
+		routes.add({
+			path: 'login',
+			element: <PasswordLogin />,
+		});
+		routes.add({
+			path: 'challenge',
+			element: <Challenge />,
+		});
+	}
+
+	if (authMethods.includes(AuthenticationMethod.FORGOTTEN_PASSWORD)) {
+		routes.add({
+			path: 'reset-password',
+			element: <ResetPassword />,
+		});
+		routes.add({
+			path: 'forgot-password',
+			element: <ForgottenPassword />,
+		});
+	}
+
 	return [
 		{
 			path: '/auth',
 			element: <Auth />,
-			children: [
-				{
-					path: 'login',
-					element: <PasswordLogin />,
-				},
-				{
-					path: 'challenge',
-					element: <Challenge />,
-				},
-				{
-					path: 'reset-password',
-					element: <ResetPassword />,
-				},
-				{
-					path: 'forgot-password',
-					element: <ForgottenPassword />,
-				},
-			],
+			children: Array.from(routes),
 		},
 	];
 };
