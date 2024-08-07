801138195f49630770b273cddd32351c72d4bc15
Steve Keep <stephenkeep+github@gmail.com>
2024-08-07 14:39:40 +1000

    return null

--- src/packages/auth-ui-components/src/components/sign-out/component.tsx
+++ src/packages/auth-ui-components/src/components/sign-out/component.tsx
@@ -4,7 +4,7 @@ import { Logout } from '../logout';
 
 export const SignOut = () => {
 	const config = import.meta.env.VITE_GRAPHWEAVER_CONFIG;
-	if (!config.auth) return [];
+	if (!config.auth) return null;
 
 	if (config.auth.primaryMethods.includes(PrimaryAuthMethod.AUTH_ZERO)) {
 		return <Auth0Logout redirectTo={`${window.location.origin}/auth/login`} />;
