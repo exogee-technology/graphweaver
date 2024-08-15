c19d83def38f8a65beb7f357b074c9d84c6b7b3e
Kevin Brown <kevin.brown@exogee.com>
2024-08-15 10:58:17 +1000

    Do the throwing earlier in the request lifecycle so we can run less code to serve bad operations.

--- src/packages/auth/src/authentication/apollo/plugin.ts
+++ src/packages/auth/src/authentication/apollo/plugin.ts
@@ -194,7 +194,7 @@ export const authApolloPlugin = <R>(
 				});
 				upsertAuthorizationContext(contextValue);
 			} else {
-				// Case 3. There is a valid auth header
+				// Case 3. There is an auth header, is it valid?
 				logger.trace('Got a token, checking it is valid.');
 
 				const tokenProvider = new AuthTokenProvider();
@@ -224,7 +224,7 @@ export const authApolloPlugin = <R>(
 			}
 
 			return {
-				didResolveOperation: async () => {
+				didResolveSource: async () => {
 					// We throw these from here instead of above so our willSendResponse will actually get called.
 					// If thrown above, we don't get to return the additional handlers, then the plugin doesn't
 					// get to handle the response.
