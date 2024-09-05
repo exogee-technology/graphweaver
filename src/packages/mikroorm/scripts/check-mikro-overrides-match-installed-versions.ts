2b6d4505cbea0b55335bc6bf6116450095fc66e8
Kevin Brown <kevin.brown@exogee.com>
2024-09-05 16:33:19 +1000

    Calm down ESLint.

--- src/packages/mikroorm/scripts/check-mikro-overrides-match-installed-versions.ts
+++ src/packages/mikroorm/scripts/check-mikro-overrides-match-installed-versions.ts
@@ -1,3 +1,5 @@
+// We're fine with require() in this file.
+/* eslint-disable @typescript-eslint/no-require-imports */
 const packageVersion = require('../package.json').devDependencies['@mikro-orm/core'];
 const overrideVersion = require('../../../package.json').pnpm.overrides['@mikro-orm/core'];
 
