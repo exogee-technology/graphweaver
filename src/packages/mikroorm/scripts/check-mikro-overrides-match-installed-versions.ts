27d02999f0bfdff8d332c9fd6cff70b4a258c068
Kevin Brown <kevin.brown@exogee.com>
2024-09-05 16:30:30 +1000

    Add script to cause an error if the overridden version of @mikro-orm/core differs from what we have in our package.json.

---
+++ src/packages/mikroorm/scripts/check-mikro-overrides-match-installed-versions.ts
@@ -0,0 +1,12 @@
+const packageVersion = require('../package.json').devDependencies['@mikro-orm/core'];
+const overrideVersion = require('../../../package.json').pnpm.overrides['@mikro-orm/core'];
+
+console.log(
+	`Checking @mikro-orm/core version. Installed: '${packageVersion}', Overridden: '${overrideVersion}'`
+);
+
+if (packageVersion !== overrideVersion) {
+	throw new Error('@mikro-orm/core version mismatch.');
+} else {
+	console.log('Success: versions match.');
+}
