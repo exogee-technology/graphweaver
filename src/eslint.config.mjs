98ef3232e8674d5f2ba23097fd1896b6f8a1c5bf
Kevin Brown <kevin.brown@exogee.com>
2024-11-24 12:10:56 +1100

    Removed eslint-plugin-oxlint and fixed types issue with eslint-config-prettier.

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import ESLint from '@eslint/js';
 import ESLintConfigPrettier from 'eslint-config-prettier';
-import Oxlint from 'eslint-plugin-oxlint';
 import globals from 'globals';
 import TypeScriptESLint from 'typescript-eslint';
 
@@ -16,7 +15,6 @@ export default TypeScriptESLint.config(
 	},
 	ESLint.configs.recommended,
 	...TypeScriptESLint.configs.recommended,
-	Oxlint.configs['flat/recommended'],
 	ESLintConfigPrettier,
 	{
 		files: ['**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}'],
@@ -68,72 +66,3 @@ export default TypeScriptESLint.config(
 		},
 	}
 );
-// module.exports = {
-// 	languageOptions: { parser: '@typescript-eslint/parser' },
-// 	plugins: { '@typescript-eslint': typescript },
-// 	files: [
-// 		'packages/**/*.ts',
-// 		'packages/**/*.tsx',
-// 		'packages/**/*.js',
-// 		'packages/**/*.jsx',
-// 		'examples/**/*.ts',
-// 		'examples/**/*.tsx',
-// 		'examples/**/*.js',
-// 		'examples/**/*.jsx',
-// 	],
-// 	ignores: [
-// 		'**/node_modules/**',
-// 		'**/lib/**',
-// 		'**/build/**',
-// 		'**/dist/**',
-// 		'**/.graphweaver/**',
-// 		'packages/cli/bin/index.js',
-// 		'packages/cli/test-init.js',
-// 		'**/app/**',
-// 		'**/local_modules/**',
-// 	],
-// 	extends: [
-// 		'eslint:recommended',
-// 		'plugin:@typescript-eslint/recommended',
-// 		'plugin:prettier/recommended',
-// 	],
-// 	env: {
-// 		es6: true,
-// 		node: true,
-// 	},
-// 	rules: {
-// 		// We are never going to fiddle with our object prototypes and
-// 		// see writing Object.prototype.hasOwnProperty.call(foo, 'bar');
-// 		// as unnecessarily verbose compared to
-// 		// foo.hasOwnProperty('bar');
-// 		'no-prototype-builtins': 'off',
-
-// 		'@typescript-eslint/no-unused-vars': [
-// 			'error',
-// 			{
-// 				// Any variable names that start with _ in an array destructure are meant to be placeholders.
-// 				destructuredArrayIgnorePattern: '^_',
-
-// 				// We want to error on unused variables but we don't want to error
-// 				// when they're part of a desctructure, as that's often used to pull out certain
-// 				// keys, or the second element of an array, etc.
-// 				ignoreRestSiblings: true,
-// 			},
-// 		],
-
-// 		// While it'd be nice to enforce this in general it gets confused with
-// 		// decorators and is otherwise very noisy.
-// 		'@typescript-eslint/explicit-module-boundary-types': 'off',
-
-// 		// When you use 'any' explicitly we trust you. If the best type isn't
-// 		// 'any' this should be picked up in a code review at this stage.
-// 		// We can enable this rule later if we want it.
-// 		'@typescript-eslint/no-explicit-any': 'off',
-
-// 		// Empty functions can actually be quite handy.
-// 		'@typescript-eslint/no-empty-function': 'off',
-
-// 		// As can empty interfaces (indeed we need them to type reference entities in Mikro)
-// 		'@typescript-eslint/no-empty-interface': 'off',
-// 	},
-// };
