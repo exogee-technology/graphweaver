2257b74f0daa4b7373c137920440f35f4664180a
Kevin Brown <kevin.brown@exogee.com>
2025-01-29 15:45:34 +1100

    Allow users to override all CDK properties for the database.

--- src/packages/cdk/src/app/database.ts
+++ src/packages/cdk/src/app/database.ts
@@ -15,8 +15,6 @@ export class DatabaseStack extends cdk.NestedStack {
 			throw new Error('Missing required database configuration');
 		}
 
-		const vpc = config.network.vpc;
-
 		this.dbInstance = new rds.DatabaseInstance(this, `${id}Database`, {
 			engine: rds.DatabaseInstanceEngine.postgres({
 				version: config.database.version ?? rds.PostgresEngineVersion.VER_16_2,
@@ -26,13 +24,16 @@ export class DatabaseStack extends cdk.NestedStack {
 				ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO),
 			instanceIdentifier: cdk.PhysicalName.GENERATE_IF_NEEDED,
 			credentials: rds.Credentials.fromGeneratedSecret(config.database.username),
-			vpc,
+			vpc: config.network.vpc,
 			storageEncrypted: true,
 			vpcSubnets: {
 				subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
 			},
 			databaseName: config.database.name,
 			securityGroups: [config.network.databaseSecurityGroup],
+
+			// Allow them to override the default settings
+			...config.database,
 		});
 	}
 }
