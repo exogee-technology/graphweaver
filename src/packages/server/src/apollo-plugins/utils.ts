1ab9acb84644eaf07a58506324e7908c0d3902c1
Juan Castano <juan871003@gmail.com>
2025-02-08 22:49:28 +1100

    Rename getOperation to getEntityOperation and refactor variable extraction logic for improved clarity and functionality

--- src/packages/server/src/apollo-plugins/utils.ts
+++ src/packages/server/src/apollo-plugins/utils.ts
@@ -23,7 +23,7 @@ type VariableValues = {
 	[name: string]: any;
 };
 
-const getOperation = (operationTypeNode: OperationTypeNode, selectionNameValue: string) => {
+const getEntityOperation = (operationTypeNode: OperationTypeNode, selectionNameValue: string) => {
 	if (operationTypeNode === OperationTypeNode.QUERY) {
 		return graphweaverMetadata.getAdditionalQueryByName(selectionNameValue);
 	}
@@ -35,18 +35,41 @@ const getOperation = (operationTypeNode: OperationTypeNode, selectionNameValue: 
 	return undefined;
 };
 
-const getVariables = (selection: SelectionNode, allVariables: VariableValues | undefined) => {
+/**
+ * Given a AST node, returns the variables that are used in that node.
+ * For example, if we have a query like:
+ * query myQueryForBooksAndAuthors($bookIds: [ID!]!, $authorIds: [ID!]!) {
+ *  books(ids: $bookIds) {
+ * 		title
+ * 	}
+ * 	authors(ids: $authorIds) {
+ * 		name
+ * 	}
+ * }
+ * then we call this functions with the AST node for `books` then we should get the variables related to that entity, not to the `authors` entity.
+ * So, if we have the variables being:
+ * {
+ * 	"bookIds": ["1", "2"],
+ * 	"authorIds": ["3", "4"]
+ * }
+ * then we should get:
+ * { "bookIds": ["1", "2"] }
+ */
+const extractVariablesFromSelection = (
+	selection: SelectionNode,
+	allVariables: VariableValues | undefined
+) => {
 	if (!allVariables) return allVariables;
 
 	const variables: VariableValues = {};
 
 	if (selection.kind !== Kind.FIELD) return variables;
 
-	const theArguments = selection.arguments;
+	const selectionArguments = selection.arguments;
 
-	if (!theArguments) return {};
+	if (!selectionArguments) return {};
 
-	theArguments.forEach((argument) => {
+	selectionArguments.forEach((argument) => {
 		if (argument.value.kind === Kind.VARIABLE) {
 			const key = argument.value.name.value;
 			if (key in allVariables) {
@@ -68,18 +91,23 @@ export const getDidResolveOperationItemsToLog = (
 	operation: OperationDefinitionNode,
 	variables: VariableValues | undefined
 ): GetDidResolveOperationItemsToLogResultItem[] => {
+	// each operation can have multiple selections, for example: query { books { title } authors { name } } => books and authors are selections
 	const allSelections = operation.selectionSet.selections;
+	// operationTypeNode is either QUERY or MUTATION
 	const operationTypeNode = operation.operation;
 
 	const result: GetDidResolveOperationItemsToLogResultItem[] = [];
 
+	// for each selection, we get the entity operation and call the logOnDidResolveOperation function if it exists
 	allSelections.forEach((selection) => {
 		const selectionNameValue = selection.kind === Kind.FIELD ? selection.name.value : undefined;
 
 		if (!selectionNameValue) return;
 
-		const additionalOperation = getOperation(operationTypeNode, selectionNameValue);
+		const entityOperation = getEntityOperation(operationTypeNode, selectionNameValue);
 
+		// build a new AST with only the current selection so we can call the logOnDidResolveOperation function (if any) for the relevant entity and with the relevant variables instead of calling that function with the whole operation and all variables.
+		// that is, if we have `query { books { title } authors { name } }` and the books entity has a logOnDidResolveOperation function, we want to call that function with the query `query { books { title } }` and the relevant variables. Not with the whole operation and all variables.
 		const ast = {
 			...operation,
 			selectionSet: {
@@ -87,19 +115,20 @@ export const getDidResolveOperationItemsToLog = (
 				selections: [selection],
 			},
 		};
-		if (additionalOperation?.logOnDidResolveOperation) {
-			const resultToLog = additionalOperation.logOnDidResolveOperation?.({
+		if (entityOperation?.logOnDidResolveOperation) {
+			const resultToLog = entityOperation.logOnDidResolveOperation?.({
 				ast,
-				variables: getVariables(selection, variables),
+				variables: extractVariablesFromSelection(selection, variables),
 			});
 			result.push({
 				query: resultToLog.query,
 				variables: JSON.stringify(resultToLog.variables),
 			});
 		} else {
+			// No custom logging function, just log the query and variables as is.
 			result.push({
 				query: stripIgnoredCharacters(print(ast)),
-				variables: JSON.stringify(getVariables(selection, variables)),
+				variables: JSON.stringify(extractVariablesFromSelection(selection, variables)),
 			});
 		}
 	});
