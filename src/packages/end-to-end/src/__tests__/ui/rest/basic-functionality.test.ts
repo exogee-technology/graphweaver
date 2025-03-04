fdba028f91b243450088ad63b483f4add1cd452e
Kevin Brown <kevin.brown@exogee.com>
2025-03-04 17:09:01 +1100

    No longer need to click the datasource.

--- src/packages/end-to-end/src/__tests__/ui/rest/basic-functionality.test.ts
+++ src/packages/end-to-end/src/__tests__/ui/rest/basic-functionality.test.ts
@@ -33,7 +33,6 @@ test('Ensure vehicles can be listed and that they have pilots', async ({ page })
 
 test('Ensure people can be filtered', async ({ page }) => {
 	await page.goto(config.adminUiUrl);
-	await page.getByRole('link', { name: 'REST (swapi.info)' }).click();
 	await page.getByTestId('Person-entity-link').click();
 	await page.getByTestId('url-filter').getByRole('combobox').click();
 	await page.getByRole('option', { name: '4', exact: true }).click();
