478a556d580a83fd4983739d9480c183ae842ed4
Kevin Brown <kevin.brown@exogee.com>
2025-03-24 11:33:09 +1100

    What if we try the new way?

--- src/packages/end-to-end/src/__tests__/api/auth/web3/challenge.test.ts
+++ src/packages/end-to-end/src/__tests__/api/auth/web3/challenge.test.ts
@@ -13,7 +13,7 @@ import Graphweaver from '@exogee/graphweaver-server';
 import assert from 'assert';
 import gql from 'graphql-tag';
 import Web3Token from 'web3-token';
-import { Mnemonic, HDNodeWallet, JsonRpcProvider, Wallet } from 'ethers';
+import ethers from 'ethers';
 import { BaseDataProvider } from '@exogee/graphweaver';
 
 // ESLint, I know it looks like the entities in this file aren't used, but they actually are.
@@ -22,10 +22,9 @@ import { BaseDataProvider } from '@exogee/graphweaver';
 
 const phrase =
 	'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about';
-const mnemonic = Mnemonic.fromPhrase(phrase);
-const mnemonic_instance = HDNodeWallet.fromMnemonic(mnemonic);
-const ethers_provider = new JsonRpcProvider();
-const ethers_signer = new Wallet(mnemonic_instance.privateKey, ethers_provider);
+const mnemonic_instance = ethers.Wallet.fromMnemonic(phrase);
+const ethers_provider = new ethers.providers.JsonRpcProvider();
+const ethers_signer = new ethers.Wallet(mnemonic_instance.privateKey, ethers_provider);
 
 class WalletAddressBackendProvider extends BaseDataProvider<
 	AuthenticationBaseEntity<WalletAddress>
