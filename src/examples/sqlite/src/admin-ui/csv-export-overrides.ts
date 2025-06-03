e90770ac2963066b1f2282f32b2627a6a5645a89
Kevin Brown <kevin.brown@exogee.com>
2025-06-03 17:25:28 +1000

    Added more usage info.

--- src/examples/sqlite/src/admin-ui/csv-export-overrides.ts
+++ src/examples/sqlite/src/admin-ui/csv-export-overrides.ts
@@ -12,9 +12,10 @@ export const csvExportOverrides = {
 						value: trackId
 						label: name
 
-						# This is the new part. It allows us to load the album title for each track.
+						# This is the new part. It allows us to load the album ID for each track.
+						# You can get as deeply nested as you need here, grab any information you'd like.
 						album {
-							title
+							albumId
 						}
 					}
 				}
@@ -31,14 +32,21 @@ export const csvExportOverrides = {
 			return csvExportRows.map((row) => {
 				delete row.__typename;
 
+				const uniqueTracks = new Set<string>();
 				const uniqueAlbums = new Set<string>();
+
 				for (const track of row.tracks) {
-					uniqueAlbums.add(track.album.title);
+					uniqueTracks.add(track.value);
+					uniqueAlbums.add(track.album.albumId);
 				}
 
 				return {
 					...row,
+					'Track Count': uniqueTracks.size,
 					'Album Count': uniqueAlbums.size,
+
+					// You can also derive any data you need from the data you've loaded.
+					'Average Tracks Per Album': uniqueTracks.size / uniqueAlbums.size,
 				};
 			});
 		},
