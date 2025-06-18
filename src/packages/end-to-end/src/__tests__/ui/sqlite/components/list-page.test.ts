fb458beacaabe271715651b3e94a8a6dbbbaf17f
Kevin Brown <kevin.brown@exogee.com>
2025-06-18 16:33:29 +1000

    Update test now that the introspected project is aliasing datasources.

--- src/packages/end-to-end/src/__tests__/ui/sqlite/components/list-page.test.ts
+++ src/packages/end-to-end/src/__tests__/ui/sqlite/components/list-page.test.ts
@@ -5,6 +5,6 @@ test('List Page', async ({ page }) => {
 	await page.goto(config.adminUiUrl);
 	await page.getByRole('link', { name: 'Artist' }).click();
 
-	await expect(page.getByText('From mikro-orm-sqlite (275 rows)')).toBeVisible();
+	await expect(page.getByText('From SQLite (275 rows)')).toBeVisible();
 	await expect(page.getByRole('cell', { name: 'Plays Metallica By Four Cellos' })).toBeVisible();
 });
