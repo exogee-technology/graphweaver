93f1061e4eab145552d6da85e28206b57580621d
Eddie Sholl <eddie.sholl@exogee.com>
2025-08-11 20:22:13 +1000

    Make sure new field requiredForUpdate is in public types

--- src/packages/core/src/decorators/field.ts
+++ src/packages/core/src/decorators/field.ts
@@ -153,6 +153,8 @@ export interface FieldOptions {
 	apiOptions?: {
 		// This marks the field as read only in the API.
 		excludeFromBuiltInWriteOperations?: boolean;
+		// This marks the field as required for update in the API.
+		requiredForUpdate?: boolean;
 	};
 
 	// This can be used by any plugin to store additional information
