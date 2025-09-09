b4ad5af24e7a73642476a32fc86cef73b6f2602a
Kevin Brown <kevin.brown@exogee.com>
2025-09-09 18:29:59 +1000

    Added link functionality.

--- src/packages/admin-ui-components/src/detail-panel/fields/relationship-count-field.tsx
+++ src/packages/admin-ui-components/src/detail-panel/fields/relationship-count-field.tsx
@@ -1,18 +1,27 @@
 import { useField, useFormikContext } from 'formik';
 
-import { EntityField, routeFor, useSchema } from '../../utils';
+import { Entity, EntityField, routeFor, useSchema } from '../../utils';
 import { useLocation, useSearchParams } from 'wouter';
 import { useCallback } from 'react';
 
 // This field is used when a relationship has too many items to even fetch them all,
 // so we just show a count of the items with a link to the table filtered to those matching
 // items instead.
-export const RelationshipCountField = ({ name, field }: { name: string; field: EntityField }) => {
+export const RelationshipCountField = ({
+	name,
+	field,
+	entity,
+}: {
+	name: string;
+	field: EntityField;
+	entity: Entity;
+}) => {
 	const { dirty } = useFormikContext();
 	const [, setLocation] = useLocation();
 	const { entityByType } = useSchema();
 	const relatedEntity = entityByType(field.type);
-	const [_, meta] = useField({ name: name, multiple: false });
+	const [_, meta] = useField({ name, multiple: false });
+	const [primaryKey] = useField({ name: entity.primaryKeyField, multiple: false });
 	const { initialValue: formEntity } = meta;
 	const [__, setSearchParams] = useSearchParams();
 
@@ -26,7 +35,17 @@ export const RelationshipCountField = ({ name, field }: { name: string; field: E
 			// Handle if the form has changed when clicking a link, if it has pop up a confirmation modal
 			e.preventDefault();
 
-			const route = routeFor({ type: field.type, id: formEntity.value as string });
+			// Figure out the property that points the other direction, e.g. back at us.
+			// If we're on Genre and we're showing a count of tracks, clicking needs to filter
+			// tracks { genre : { id: 1 } }
+			const inverseRelationship = relatedEntity.fields.find((field) => field.type === entity.name);
+
+			if (!inverseRelationship) return;
+
+			const route = routeFor({
+				type: field.type,
+				filters: { [inverseRelationship.name]: { [entity.primaryKeyField]: primaryKey.value } },
+			});
 
 			if (dirty) {
 				// Push the route to this link onto the searchParams
