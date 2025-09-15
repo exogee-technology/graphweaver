56541bd9c496b2454ace502db482d46ed359553b
Kevin Brown <kevin.brown@exogee.com>
2025-09-15 17:19:04 +1000

    Upgrade to apollo client v4.

--- src/packages/admin-ui-components/src/apollo.ts
+++ src/packages/admin-ui-components/src/apollo.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache, ApolloLink, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloLink, HttpLink, Observable } from '@apollo/client';
 import { inflate } from 'graphql-deduplicator';
 import { localStorageAuthKey, uri } from './config';
 
@@ -35,37 +35,43 @@ const authLink = new ApolloLink((operation, forward) => {
 		},
 	});
 
-	return forward(operation).map((response) => {
-		const context = operation.getContext();
-		// If the server sends back a header called `X-Auth-Redirect` on any response, we need to redirect the user to that URL
-		// unless we're already there.
-		const redirectHeader = context.response.headers.get('X-Auth-Redirect');
-		if (redirectHeader && redirectHeader !== window.location.href) {
-			window.location.href = redirectHeader;
-		} else if (redirectHeader && redirectHeader === window.location.href) {
-			console.warn(
-				"Received redirect header from server but we're already at the redirect URL, no need to redirect."
-			);
-		}
+	return new Observable((observer) => {
+		return forward(operation).subscribe({
+			next: (response) => {
+				const context = operation.getContext();
+				// If the server sends back a header called `X-Auth-Redirect` on any response, we need to redirect the user to that URL
+				// unless we're already there.
+				const redirectHeader = context.response.headers.get('X-Auth-Redirect');
+				if (redirectHeader && redirectHeader !== window.location.href) {
+					window.location.href = redirectHeader;
+				} else if (redirectHeader && redirectHeader === window.location.href) {
+					console.warn(
+						"Received redirect header from server but we're already at the redirect URL, no need to redirect."
+					);
+				}
 
-		// If the server sends back a header called `Authorization` on any response, we need to
-		// update our `graphweaver-auth` local storage value with what we got from the server.
-		const newAuthToken = context.response.headers.get('Authorization');
+				// If the server sends back a header called `Authorization` on any response, we need to
+				// update our `graphweaver-auth` local storage value with what we got from the server.
+				const newAuthToken = context.response.headers.get('Authorization');
 
-		if (newAuthToken) {
-			// The token should include the type and the credential, if not let's emit a warning.
-			if (newAuthToken.split(' ').length < 2)
-				console.warn(
-					'New Graphweaver Auth Token is invalid, it should be in the form "[type] [credential]"'
-				);
-			localStorage.setItem(localStorageAuthKey, newAuthToken);
-		}
+				if (newAuthToken) {
+					// The token should include the type and the credential, if not let's emit a warning.
+					if (newAuthToken.split(' ').length < 2)
+						console.warn(
+							'New Graphweaver Auth Token is invalid, it should be in the form "[type] [credential]"'
+						);
+					localStorage.setItem(localStorageAuthKey, newAuthToken);
+				}
 
-		// Inflate the response data, this is deduplicated by default
-		if (response.data) {
-			response.data = inflate(response.data);
-		}
-		return response;
+				// Inflate the response data, this is deduplicated by default
+				if (response.data) {
+					response.data = inflate(response.data);
+				}
+				observer.next(response);
+			},
+			error: (error) => observer.error(error),
+			complete: () => observer.complete(),
+		});
 	});
 });
 
