a9a15a6acf506a57e78b5e913683edb7bc084424
Mack Cheesman <mack.cheesman@exogee.com>
2026-02-03 11:08:10 +1100

    Created a mutation that replicates the behaviour encountered in 473

--- src/examples/client-side-keys/src/backend/schema/user.ts
+++ src/examples/client-side-keys/src/backend/schema/user.ts
@@ -1,4 +1,12 @@
-import { Entity, Field, ID, RelationshipField } from '@exogee/graphweaver';
+import {
+	Entity,
+	Field,
+	ID,
+	RelationshipField,
+	EntityMetadata,
+	fromBackendEntity,
+	graphweaverMetadata,
+} from '@exogee/graphweaver';
 import { MikroBackendProvider } from '@exogee/graphweaver-mikroorm';
 import { Task } from './task';
 import { User as OrmUser } from '../entities';
@@ -23,3 +31,28 @@ export class User {
 	@RelationshipField<Task>(() => [Task], { relatedField: 'user' })
 	tasks!: Task[];
 }
+
+graphweaverMetadata.addMutation({
+	name: 'exampleMutation',
+	getType: () => [User],
+	resolver: async () => {
+		const userEntity = graphweaverMetadata.getEntityByName('User') as EntityMetadata<User, OrmUser>;
+
+		const user = await userEntity.provider.createOne({
+			id: '1',
+			username: 'example_mutation',
+			email: 'example@test.com',
+			tasks: [
+				{
+					id: '1',
+					description: 'Wash your face with orange juice',
+				},
+				{
+					id: '2',
+					description: 'Clean your teeth with bubblegum',
+				},
+			],
+		} as unknown as Partial<OrmUser>);
+		return fromBackendEntity(userEntity, user);
+	},
+});
