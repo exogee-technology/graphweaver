1d109c58762008713d92c279bbd5c72ac8edd3e5
Daniel Givney <dgivney@ubuntu-macbook.local>
2026-02-12 15:42:07 +1100

    EXOGW-340 - Add config for MySQL

--- src/packages/end-to-end/src/utils.ts
+++ src/packages/end-to-end/src/utils.ts
@@ -45,8 +45,8 @@ export const resetDatabase = async () => {
 		const connection = await mysql.createConnection({
 			host: process.env.DATABASE_HOST || 'localhost',
 			port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 3306,
-			user: process.env.DATABASE_USERNAME || 'mysql',
-			password: process.env.DATABASE_PASSWORD || 'mysql',
+			user: process.env.DATABASE_USERNAME || 'root',
+			password: process.env.DATABASE_PASSWORD || 'root',
 			multipleStatements: true,
 		});
 
