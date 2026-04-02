7019707a6f69c31b727459c974bc8d39df3c2cdc
Kevin Brown <kevin.brown@exogee.com>
2026-04-02 13:55:18 +1100

    Revert "Remove useless cast."
    This reverts commit dae2a6721f00a892c9b69c6113271021a5ca3dab.

--- src/packages/mikroorm/src/database.ts
+++ src/packages/mikroorm/src/database.ts
@@ -69,7 +69,7 @@ class DatabaseImplementation {
 	public get em() {
 		const tx = this.transactionalAsyncLocal.getStore();
 		if (tx) return tx.em;
-		return this.orm.em;
+		return this.orm.em as EntityManager;
 	}
 
 	public async transactional<T>(
